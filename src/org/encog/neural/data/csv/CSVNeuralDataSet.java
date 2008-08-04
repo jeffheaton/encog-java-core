aee5af23edd914d571118f3f448fb372d15618dd
Jeff Heaton <jeff@jeffheaton.com>
2008-08-04 21:50:44 +0000

    Issue-10: Added CSV support
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@60 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/neural/data/csv/CSVNeuralDataSet.java
+++ src/org/encog/neural/data/csv/CSVNeuralDataSet.java
@@ -1,15 +1,15 @@
 package org.encog.neural.data.csv;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Iterator;
 
+import org.encog.neural.NeuralNetworkError;
 import org.encog.neural.data.NeuralData;
 import org.encog.neural.data.NeuralDataError;
 import org.encog.neural.data.NeuralDataPair;
 import org.encog.neural.data.NeuralDataSet;
+import org.encog.neural.data.basic.BasicNeuralData;
+import org.encog.neural.data.basic.BasicNeuralDataPair;
 import org.encog.util.ReadCSV;
 
 public class CSVNeuralDataSet implements NeuralDataSet {
@@ -20,37 +20,90 @@ public class CSVNeuralDataSet implements NeuralDataSet {
 	private int inputSize;
 	private int idealSize;
 	private char delimiter;
+	private boolean headers;
 	
 	public class CSVNeuralIterator implements Iterator<NeuralDataPair>
 	{
 		private ReadCSV reader;
+		private boolean dataReady;
 		
 		public CSVNeuralIterator()
 		{
 			try {
 				this.reader = null;
-				this.reader = new ReadCSV(filename);
+				this.reader = new ReadCSV(filename,headers,delimiter);
+				dataReady = false;
 			} catch (IOException e) {
-				// if this happens just allow reader to remain null
+				throw new NeuralNetworkError(e);
 			}
 		}
+		
 		
 		public boolean hasNext() {
 			if( this.reader==null )
 				return false;
-			return true;
+			
+			if( dataReady )
+				return true;
+			
+			try
+			{
+				if( reader.next() )
+				{
+					dataReady = true;
+					return true;
+				}
+				else 
+				{
+					dataReady = false;
+					return false;
+				}
+			}
+			catch(IOException e)
+			{
+				throw new NeuralNetworkError(e);
+			}
 			
 		}
 
-		public NeuralDataPair next() {
-			// TODO Auto-generated method stub
-			return null;
+		public NeuralDataPair next()  {
+						
+			NeuralData input = new BasicNeuralData(inputSize);
+			NeuralData ideal = null;
+			
+			for(int i=0;i<inputSize;i++)
+			{
+				input.setData(i, reader.getDouble(i));
+			}
+			
+			if( idealSize>0 )
+			{
+				ideal = new BasicNeuralData(idealSize);
+				for(int i=0;i<idealSize;i++)
+				{
+					ideal.setData(i, reader.getDouble(i+inputSize));
+				}
+				 
+			}
+			
+			this.dataReady = false;		
+			return new BasicNeuralDataPair(input,ideal);
 		}
 
 		public void remove() {
-			// TODO Auto-generated method stub
+			try {
+				this.reader.close();
+			} catch (IOException e) {
+				// Not much we can do at this point, and throwing will
+				// break the interface.
+			}
 			
 		}	
+	}
+	
+	public CSVNeuralDataSet(String filename, int inputSize,int idealSize)
+	{
+		this(filename,inputSize,idealSize,',');
 	}
 	
 	public CSVNeuralDataSet(String filename, int inputSize,int idealSize,char delimiter)
@@ -59,6 +112,7 @@ public class CSVNeuralDataSet implements NeuralDataSet {
 		this.inputSize = inputSize;
 		this.idealSize = idealSize;
 		this.delimiter = delimiter;
+		this.headers = headers;
 	}
 
 	public void add(NeuralData inputData, NeuralData idealData) {
@@ -97,7 +151,6 @@ public class CSVNeuralDataSet implements NeuralDataSet {
 	}
 
 	public void add(NeuralData data1) {
-		// TODO Auto-generated method stub
-		
+		throw new NeuralDataError(CSVNeuralDataSet.ADD_NOT_SUPPORTED);		
 	}
 }
