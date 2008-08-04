7b601a5586347d5e040fb00b544b2d67fc55ecda
Jeff Heaton <jeff@jeffheaton.com>
2008-08-04 21:51:49 +0000

    Issue-10: Added CSV support
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@62 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- test/org/encog/neural/XOR.java
+++ test/org/encog/neural/XOR.java
@@ -1,6 +1,10 @@
 package org.encog.neural;
 
+import junit.framework.TestCase;
+
 import org.encog.neural.data.NeuralData;
+import org.encog.neural.data.NeuralDataPair;
+import org.encog.neural.data.NeuralDataSet;
 import org.encog.neural.data.basic.BasicNeuralData;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.FeedforwardLayer;
@@ -29,6 +33,27 @@ public class XOR {
 			}
 			
 			return true;
+		}
+		
+		public static void testXORDataSet(NeuralDataSet set)
+		{
+			int row = 0;
+			for(NeuralDataPair item: set)
+			{
+				for(int i=0;i<XOR.XOR_INPUT[0].length;i++)
+				{
+					TestCase.assertEquals(item.getInput().getData(i), 
+							XOR.XOR_INPUT[row][i]);
+				}
+				
+				for(int i=0;i<XOR.XOR_IDEAL[0].length;i++)
+				{
+					TestCase.assertEquals(item.getIdeal().getData(i), 
+							XOR.XOR_IDEAL[row][i]);
+				}
+				
+				row++;
+			}
 		}
 		
 		public static BasicNetwork createThreeLayerNet()
