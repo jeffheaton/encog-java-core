5720ce36847876078bf3eb39b1c8a68d3c8a9e62
Jeff Heaton <jeff@jeffheaton.com>
2008-08-06 17:46:31 +0000

    Issue-8: data sources should support XML
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@66 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/neural/data/xml/XMLNeuralDataSet.java
+++ src/org/encog/neural/data/xml/XMLNeuralDataSet.java
@@ -5,98 +5,85 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.encog.bot.html.ParseHTML;
+import org.encog.neural.NeuralNetworkError;
 import org.encog.neural.NeuralNetworkException;
 import org.encog.neural.data.NeuralData;
+import org.encog.neural.data.NeuralDataError;
 import org.encog.neural.data.NeuralDataPair;
 import org.encog.neural.data.NeuralDataSet;
+import org.encog.neural.data.basic.BasicNeuralDataSet;
+import org.encog.neural.data.csv.CSVNeuralDataSet;
+import org.encog.neural.persist.persistors.BasicNeuralDataSetPersistor;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
-public class XMLNeuralDataSet implements NeuralDataSet {
+public class XMLNeuralDataSet extends BasicNeuralDataSet {
 
-	public class XMLNeuralDataSetIterator implements Iterator<NeuralDataPair>
+	public final static String ADD_NOT_SUPPORTED = "Adds are not supported with this dataset, it is read only.";
+		
+	public XMLNeuralDataSet(
+			String filename,
+			String pairXML,
+			String inputXML,
+			String idealXML,
+			String valueXML) throws NeuralNetworkException
 	{
-		public boolean hasNext() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		public NeuralDataPair next() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public void remove() {
-			// TODO Auto-generated method stub
+				
+		try {
+			InputStream is = new FileInputStream(filename);
 			
-		}		
+			// setup the XML parser stuff
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+			DocumentBuilder db = null;
+			db = dbf.newDocumentBuilder();
+
+			Document doc = null;
+			doc = db.parse(is);
+			Element node = doc.getDocumentElement();
+
+			// read in the data
+
+			BasicNeuralDataSetPersistor persistor = new BasicNeuralDataSetPersistor();
+			
+			persistor.setIdealXML(idealXML);
+			persistor.setInputXML(inputXML);
+			persistor.setPairXML(pairXML);
+			persistor.setValueXML(valueXML);
+			
+			BasicNeuralDataSet set = (BasicNeuralDataSet) persistor.load(node);
+			this.setData(set.getData());
+
+			
+			is.close();
+
+		} catch (javax.xml.parsers.ParserConfigurationException e) {
+			throw new NeuralNetworkException(e);
+		} catch (org.xml.sax.SAXException e) {
+			throw new NeuralNetworkException(e);
+		} catch (java.io.IOException e) {
+			throw new NeuralNetworkException(e);
+		}	
 	}
 	
 	public void add(NeuralData inputData, NeuralData idealData) {
-		// TODO Auto-generated method stub
-		
+		throw new NeuralDataError(XMLNeuralDataSet.ADD_NOT_SUPPORTED);		
 	}
 
 	public void add(NeuralDataPair inputData) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public int getIdealSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public int getInputSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public Iterator<NeuralDataPair> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		throw new NeuralDataError(XMLNeuralDataSet.ADD_NOT_SUPPORTED);		
 	}
 	
-	public void load(String filename) throws NeuralNetworkException 
-	{
-		try
-		{
-		InputStream fis = new FileInputStream(filename);
-		load(fis);
-		fis.close();
-		}
-		catch(IOException e)
-		{
-			throw new NeuralNetworkException(e);
-		}
-	}
-	
-	public void load(InputStream is) throws NeuralNetworkException 
-	{
-		try
-		{
-		
-		XMLReader xr = XMLReaderFactory.createXMLReader();
-		SAXHandler handler = new SAXHandler();
-		xr.setContentHandler(handler);
-		xr.setErrorHandler(handler);
-
-		xr.parse(new InputSource(is));
-		}
-		catch(SAXException e)
-		{
-			throw new NeuralNetworkException(e);
-		} catch (IOException e) {
-			throw new NeuralNetworkException(e);
-		}
-			
-	}
-
 	public void add(NeuralData data1) {
-		// TODO Auto-generated method stub
-		
+		throw new NeuralDataError(XMLNeuralDataSet.ADD_NOT_SUPPORTED);		
 	}
-
 }
