ae88bf9e28d9d015fbc36d3173c4453529d7b4ac
Jeff Heaton <jeff@jeffheaton.com>
2008-11-25 21:47:22 +0000

    Fixed unit tests to align with C#
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@175 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- test/org/encog/neural/data/market/TestMarketData.java
+++ test/org/encog/neural/data/market/TestMarketData.java
@@ -28,7 +28,7 @@ public class TestMarketData extends TestCase {
 		Date end = DateUtil.createDate(7, 31, 2008);
 		marketData.load(begin, end);
 		marketData.generate();
-		TestCase.assertEquals(21,marketData.getPoints().size());
+		TestCase.assertEquals(22,marketData.getPoints().size());
 		
 		// first test the points
 		Iterator<TemporalPoint> itr = marketData.getPoints().iterator();
@@ -43,21 +43,21 @@ public class TestMarketData extends TestCase {
 		point = itr.next();
 		TestCase.assertEquals(1, point.getSequence());
 		TestCase.assertEquals(4, point.getData().length);
+		TestCase.assertEquals(168.18,point.getData(0));
+		TestCase.assertEquals(25.88,point.getData(1));
+		TestCase.assertEquals(29,(int)(point.getData(2)/1000000));
+		TestCase.assertEquals(84,(int)(point.getData(3)/1000000));
+		
+		point = itr.next();
+		TestCase.assertEquals(2, point.getSequence());
+		TestCase.assertEquals(4, point.getData().length);
 		TestCase.assertEquals(170.12,point.getData(0));
 		TestCase.assertEquals(25.98,point.getData(1));
 		TestCase.assertEquals(18,(int)(point.getData(2)/1000000));
 		TestCase.assertEquals(37,(int)(point.getData(3)/1000000));
-		
-		point = itr.next();
-		TestCase.assertEquals(5, point.getSequence());
-		TestCase.assertEquals(4, point.getData().length);
-		TestCase.assertEquals(175.16,point.getData(0));
-		TestCase.assertEquals(26.03,point.getData(1));
-		TestCase.assertEquals(29,(int)(point.getData(2)/1000000));
-		TestCase.assertEquals(71,(int)(point.getData(3)/1000000));
 
 		// now check the actual data
-		TestCase.assertEquals(15,marketData.getData().size());
+		TestCase.assertEquals(16,marketData.getData().size());
 		TestCase.assertEquals(20, marketData.getInputNeuronCount());
 		TestCase.assertEquals(1, marketData.getOutputNeuronCount());
 
@@ -66,10 +66,35 @@ public class TestMarketData extends TestCase {
 		TestCase.assertEquals(20, pair.getInput().size());
 		TestCase.assertEquals(1, pair.getIdeal().size());
 		
-		TestCase.assertEquals(-0.026, Math.round(pair.getInput().getData(0)*1000.0)/1000.0);
-		TestCase.assertEquals(-0.033, Math.round(pair.getInput().getData(1)*1000.0)/1000.0);		
-		TestCase.assertEquals(-0.529, Math.round(pair.getInput().getData(2)*1000.0)/1000.0);
-		TestCase.assertEquals(-0.63, Math.round(pair.getInput().getData(3)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.037, Math.round(pair.getInput().getData(0)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.037, Math.round(pair.getInput().getData(1)*1000.0)/1000.0);		
+		TestCase.assertEquals(-0.246, Math.round(pair.getInput().getData(2)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.156, Math.round(pair.getInput().getData(3)*1000.0)/1000.0);
+		TestCase.assertEquals(0.012, Math.round(pair.getInput().getData(4)*1000.0)/1000.0);
+		TestCase.assertEquals(0.0040, Math.round(pair.getInput().getData(5)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.375, Math.round(pair.getInput().getData(6)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.562, Math.round(pair.getInput().getData(7)*1000.0)/1000.0);
+		TestCase.assertEquals(0.03, Math.round(pair.getInput().getData(8)*1000.0)/1000.0);
+		TestCase.assertEquals(0.0020, Math.round(pair.getInput().getData(9)*1000.0)/1000.0);
+		TestCase.assertEquals(0.57, Math.round(pair.getInput().getData(10)*100.0)/100.0);
+		TestCase.assertEquals(0.929, Math.round(pair.getInput().getData(11)*1000.0)/1000.0);
+		TestCase.assertEquals(0.025, Math.round(pair.getInput().getData(12)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.0070, Math.round(pair.getInput().getData(13)*1000.0)/1000.0);
+		TestCase.assertEquals(0.084, Math.round(pair.getInput().getData(14)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.084, Math.round(pair.getInput().getData(15)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.03, Math.round(pair.getInput().getData(16)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.024, Math.round(pair.getInput().getData(17)*1000.0)/1000.0);
+		TestCase.assertEquals(0.008, Math.round(pair.getInput().getData(18)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.172, Math.round(pair.getInput().getData(19)*1000.0)/1000.0);
+
+		pair = itr2.next();
+		TestCase.assertEquals(20, pair.getInput().size());
+		TestCase.assertEquals(1, pair.getIdeal().size());
+
+		TestCase.assertEquals(0.012, Math.round(pair.getInput().getData(0)*1000.0)/1000.0);
+		TestCase.assertEquals(0.0040, Math.round(pair.getInput().getData(1)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.375, Math.round(pair.getInput().getData(2)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.562, Math.round(pair.getInput().getData(3)*1000.0)/1000.0);
 		TestCase.assertEquals(0.03, Math.round(pair.getInput().getData(4)*1000.0)/1000.0);
 		TestCase.assertEquals(0.0020, Math.round(pair.getInput().getData(5)*1000.0)/1000.0);
 		TestCase.assertEquals(0.566, Math.round(pair.getInput().getData(6)*1000.0)/1000.0);
@@ -81,36 +106,11 @@ public class TestMarketData extends TestCase {
 		TestCase.assertEquals(-0.03, Math.round(pair.getInput().getData(12)*1000.0)/1000.0);
 		TestCase.assertEquals(-0.024, Math.round(pair.getInput().getData(13)*1000.0)/1000.0);
 		TestCase.assertEquals(0.0080, Math.round(pair.getInput().getData(14)*1000.0)/1000.0);
-		TestCase.assertEquals(-0.172, Math.round(pair.getInput().getData(15)*1000.0)/1000.0);
+		TestCase.assertEquals(-0.172, Math.round(pair.getInput().getData(15)*1000.0)/1000.0);		
 		TestCase.assertEquals(0.014, Math.round(pair.getInput().getData(16)*1000.0)/1000.0);
 		TestCase.assertEquals(0.0090, Math.round(pair.getInput().getData(17)*1000.0)/1000.0);
 		TestCase.assertEquals(-0.062, Math.round(pair.getInput().getData(18)*1000.0)/1000.0);
 		TestCase.assertEquals(0.066, Math.round(pair.getInput().getData(19)*1000.0)/1000.0);
-
-		pair = itr2.next();
-		TestCase.assertEquals(20, pair.getInput().size());
-		TestCase.assertEquals(1, pair.getIdeal().size());
-
-		TestCase.assertEquals(0.03, Math.round(pair.getInput().getData(0)*1000.0)/1000.0);
-		TestCase.assertEquals(0.0020, Math.round(pair.getInput().getData(1)*1000.0)/1000.0);
-		TestCase.assertEquals(0.566, Math.round(pair.getInput().getData(2)*1000.0)/1000.0);
-		TestCase.assertEquals(0.929, Math.round(pair.getInput().getData(3)*1000.0)/1000.0);
-		TestCase.assertEquals(0.025, Math.round(pair.getInput().getData(4)*1000.0)/1000.0);
-		TestCase.assertEquals(-0.0070, Math.round(pair.getInput().getData(5)*1000.0)/1000.0);
-		TestCase.assertEquals(0.084, Math.round(pair.getInput().getData(6)*1000.0)/1000.0);
-		TestCase.assertEquals(-0.084, Math.round(pair.getInput().getData(7)*1000.0)/1000.0);
-		TestCase.assertEquals(-0.03, Math.round(pair.getInput().getData(8)*1000.0)/1000.0);
-		TestCase.assertEquals(-0.024, Math.round(pair.getInput().getData(9)*1000.0)/1000.0);
-		TestCase.assertEquals(0.0080, Math.round(pair.getInput().getData(10)*1000.0)/1000.0);
-		TestCase.assertEquals(-0.172, Math.round(pair.getInput().getData(11)*1000.0)/1000.0);
-		TestCase.assertEquals(0.014, Math.round(pair.getInput().getData(12)*1000.0)/1000.0);
-		TestCase.assertEquals(0.0090, Math.round(pair.getInput().getData(13)*1000.0)/1000.0);
-		TestCase.assertEquals(-0.062, Math.round(pair.getInput().getData(14)*1000.0)/1000.0);
-		TestCase.assertEquals(0.066, Math.round(pair.getInput().getData(15)*1000.0)/1000.0);		
-		TestCase.assertEquals(-0.023, Math.round(pair.getInput().getData(16)*1000.0)/1000.0);
-		TestCase.assertEquals(-0.0080, Math.round(pair.getInput().getData(17)*1000.0)/1000.0);
-		TestCase.assertEquals(0.107, Math.round(pair.getInput().getData(18)*1000.0)/1000.0);
-		TestCase.assertEquals(0.202, Math.round(pair.getInput().getData(19)*1000.0)/1000.0);
 		
 
 		
