a2b5200986a84ffa7bad5442a93bfe8dec53127b
Jeff Heaton <jeff@jeffheaton.com>
2008-12-12 19:20:17 +0000

    Adding parse unit test
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@219 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ test/org/encog/parse/TestParse.java
@@ -0,0 +1,34 @@
+package org.encog.parse;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.encog.parse.signal.Signal;
+
+
+
+import junit.framework.TestCase;
+
+public class TestParse extends TestCase {
+	
+	public void testString() throws Throwable
+	{
+		Parse parse = new Parse();
+		parse.load();
+		Signal signal = parse.parse("Hello world");
+		Collection<Signal> list = signal.findByType("word");
+		
+		TestCase.assertEquals(2, list.size());
+		int i=0;
+		for(Signal s:list)
+		{
+			System.out.println(s.toString());
+			switch(i)
+			{
+				case 0:TestCase.assertEquals("Hello", s.toString());break;
+				case 1:TestCase.assertEquals("world", s.toString());break;
+			}
+			i++;
+		}
+	}
+}
