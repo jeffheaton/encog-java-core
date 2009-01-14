733170eba7b935cb75ed69d7f56db88d39bf6e9f
Jeff Heaton <jeff@jeffheaton.com>
2009-01-14 22:26:36 +0000

    Added the concurrency package to Encog
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@284 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/util/concurrency/EncogConcurrency.java
+++ src/org/encog/util/concurrency/EncogConcurrency.java
@@ -1,19 +1,51 @@
 package org.encog.util.concurrency;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.encog.EncogError;
+
 public class EncogConcurrency {
 
-	private int threadCount;
+	private int maxThreads;
 	private static EncogConcurrency instance;
-	
-	public static EncogConcurrency getInstance()
-	{
-		if( instance==null )
+	private ExecutorService executor;
+
+	public EncogConcurrency() {
+		setMaxThreads(0);
+	}
+
+	public static EncogConcurrency getInstance() {
+		if (instance == null)
 			instance = new EncogConcurrency();
 		return instance;
 	}
-	
-	public void processTask(EncogTask task)
-	{
-		task.run();
-	}	
+
+	public void setMaxThreads(int maxThreads) {
+		this.maxThreads = maxThreads;
+		if (this.maxThreads > 0) {
+			executor = Executors.newFixedThreadPool(maxThreads);
+		} else {
+			executor = null;
+		}
+	}
+
+	public void processTask(EncogTask task) {
+		if (executor == null)
+			task.run();
+		else
+			executor.execute(task);
+	}
+
+	public void waitForComplete(long timeout) {
+		if (executor != null) {
+			try {
+				executor.awaitTermination(timeout, TimeUnit.SECONDS);
+			} catch (InterruptedException e) {
+				throw new EncogError(e);
+			}
+		}
+
+	}
 }
