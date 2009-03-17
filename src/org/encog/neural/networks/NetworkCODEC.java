801a9d31d6c6a2a57f109645a9cd3709dd6f749d
Jeff Heaton <jeff@jeffheaton.com>
2009-03-17 06:18:20 +0000

    spain
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@367 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/neural/networks/NetworkCODEC.java
@@ -0,0 +1,84 @@
+package org.encog.neural.networks;
+
+import org.encog.neural.networks.layers.Layer;
+import org.encog.neural.networks.synapse.Synapse;
+
+public class NetworkCODEC {
+
+	/**
+	 * Private constructor.
+	 */
+	private NetworkCODEC() {
+		
+	}
+	
+	/**
+	 * Use an array to populate the memory of the neural network.
+	 * 
+	 * @param array
+	 *            An array of doubles.
+	 * @param network The network to encode.
+	 */
+	public static void arrayToNetwork(final Double[] array,
+			final BasicNetwork network) {
+
+		arrayToLayer(network.getInputLayer(),array,0);
+
+	}
+	
+	private static int arrayToLayer(final Layer layer,final Double[] array, final int index )
+	{
+		int currentIndex = index;
+		for(Synapse synapse: layer.getNext())
+		{
+			currentIndex = synapse.getMatrix().fromPackedArray(array, currentIndex);
+			arrayToLayer(synapse.getToLayer(),array,currentIndex);
+		}
+		return currentIndex;
+	}
+	
+	
+	
+	
+
+	/**
+	 * Convert to an array. This is used with some training algorithms that
+	 * require that the "memory" of the neuron(the weight and threshold values)
+	 * be expressed as a linear array.
+	 * @param network The network to encode.
+	 * @return The memory of the neuron.
+	 */
+	public static Double[] networkToArray(final BasicNetwork network) {
+		int size = 0;
+
+		// first determine size
+		for (final Synapse synapse : network.getSynapses()) {
+				size += synapse.getMatrixSize();
+		}
+
+		// allocate an array to hold
+		final Double[] result = new Double[size];
+
+		layerToArray(0,network.getInputLayer(),result);
+
+		return result;
+	}
+	
+	private static int layerToArray(final int index, Layer layer, final Double[] array)
+	{
+		int currentIndex = index;
+		
+		for(Synapse synapse: layer.getNext())
+		{
+			Double[] temp = synapse.getMatrix().toPackedArray();
+			for(int i=0;i<temp.length;i++)
+			{
+				array[currentIndex++] = temp[i];
+			}
+			currentIndex = layerToArray(currentIndex,synapse.getToLayer(),array);
+		}
+		return currentIndex;
+	}
+	
+	
+}
