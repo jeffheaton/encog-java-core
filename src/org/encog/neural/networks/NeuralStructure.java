bbce47e503aa458eabf289f8607d41fd1e93d5e9
Jeff Heaton <jeff@jeffheaton.com>
2009-03-25 23:24:00 +0000

    refactoring
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@379 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/neural/networks/NeuralStructure.java
@@ -0,0 +1,95 @@
+package org.encog.neural.networks;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.encog.neural.networks.layers.Layer;
+import org.encog.neural.networks.synapse.Synapse;
+
+public class NeuralStructure {
+	private List<Layer> layers = new ArrayList<Layer>();
+	private List<Synapse> synapses = new ArrayList<Synapse>();
+	private BasicNetwork network;
+	
+	public NeuralStructure(BasicNetwork network)
+	{
+		this.network = network;
+	}
+	
+	public void finalizeStructure()
+	{
+		finalizeLayers();
+		finalizeSynapses();		
+	}
+	
+	private void finalizeLayers()
+	{
+		Set<Layer> result = new HashSet<Layer>();
+		getLayers(result, this.network.getInputLayer());
+		this.layers.clear();
+		this.layers.addAll(result);
+	}
+	
+	private void finalizeSynapses()
+	{
+		Set<Synapse> result = new HashSet<Synapse>();
+		for (Layer layer : getLayers()) {
+			for (Synapse synapse : layer.getNext()) {
+				result.add(synapse);
+			}
+		}
+		this.synapses.clear();
+		this.synapses.addAll(result);
+	}
+	
+	private void getLayers(Set<Layer> result, Layer layer) {
+		result.add(layer);
+
+		for (Synapse synapse : layer.getNext()) {
+			Layer nextLayer = synapse.getToLayer();
+
+			if (!result.contains(nextLayer)) {
+				getLayers(result, nextLayer);
+			}
+		}
+	}
+
+	public Collection<Layer> getPreviousLayers(Layer targetLayer) {
+		Collection<Layer> result = new HashSet<Layer>();
+		for (Layer layer : this.getLayers()) {
+			for (Synapse synapse : layer.getNext()) {
+				if (synapse.getToLayer() == targetLayer) {
+					result.add(synapse.getFromLayer());
+				}
+			}
+		}
+		return result;
+	}
+
+	public Collection<Synapse> getPreviousSynapses(Layer targetLayer) {
+		Collection<Synapse> result = new HashSet<Synapse>();
+		for (Layer layer : this.getLayers()) {
+			for (Synapse synapse : layer.getNext()) {
+				if (synapse.getToLayer() == targetLayer) {
+					result.add(synapse);
+				}
+			}
+		}
+		return result;
+	}
+
+	public List<Layer> getLayers() {
+		return layers;
+	}
+
+	public List<Synapse> getSynapses() {
+		return synapses;
+	}
+
+	public BasicNetwork getNetwork() {
+		return network;
+	}
+}
