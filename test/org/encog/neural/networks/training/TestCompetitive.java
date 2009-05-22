148b05394b3b076c59b0abe9278b39019a39955a
Jeff Heaton <jeff@jeffheaton.com>
2009-05-22 11:47:38 +0000

    fixed unit tests for new SOM code
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@630 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- test/org/encog/neural/networks/training/TestCompetitive.java
+++ test/org/encog/neural/networks/training/TestCompetitive.java
@@ -1,5 +1,6 @@
 package org.encog.neural.networks.training;
 
+import org.encog.matrix.Matrix;
 import org.encog.neural.activation.ActivationLinear;
 import org.encog.neural.data.NeuralData;
 import org.encog.neural.data.NeuralDataSet;
@@ -7,9 +8,9 @@ import org.encog.neural.data.basic.BasicNeuralData;
 import org.encog.neural.data.basic.BasicNeuralDataSet;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
-import org.encog.neural.networks.synapse.SynapseType;
+import org.encog.neural.networks.layers.Layer;
+import org.encog.neural.networks.synapse.Synapse;
 import org.encog.neural.networks.training.competitive.CompetitiveTraining;
-import org.encog.neural.networks.training.competitive.CompetitiveTraining.LearningMethod;
 import org.encog.neural.networks.training.competitive.neighborhood.NeighborhoodSingle;
 import org.encog.util.logging.Logging;
 import org.junit.Assert;
@@ -19,7 +20,21 @@ public class TestCompetitive {
 
 	public static double SOM_INPUT[][] = { { 0.0, 0.0, 1.0, 1.0 },
 			{ 1.0, 1.0, 0.0, 0.0 } };
+	
+	// Just a random starting matrix, but it gives us a constant starting point
+	public static final double[][] MATRIX_ARRAY = {
+			{0.9950675732277183, -0.09315692732658198}, 
+			{0.9840257865083011, 0.5032129897356723}, 
+			{-0.8738960119753589, -0.48043680531294997}, 
+			{-0.9455207768842442, -0.8612565984447569}
+			};
 
+	private Synapse findSynapse(BasicNetwork network)
+	{
+		Layer input = network.getInputLayer();
+		return input.getNext().get(0);
+	}
+	
 	@Test
 	public void testSOM() {
 		Logging.stopConsoleLogging();
@@ -31,20 +46,21 @@ public class TestCompetitive {
 		// Create the neural network.
 		final BasicNetwork network = new BasicNetwork();
 		network.addLayer(new BasicLayer(new ActivationLinear(), false, 4));
-		network.addLayer(new BasicLayer(new ActivationLinear(),false,2),SynapseType.Normalize);
+		network.addLayer(new BasicLayer(new ActivationLinear(), false, 2));
 		network.getStructure().finalizeStructure();
 		network.reset();
+		
+		Synapse synapse = findSynapse(network);
+		synapse.setMatrix(new Matrix(MATRIX_ARRAY));
 
-		CompetitiveTraining train = new CompetitiveTraining(
-				network,
-				training,
-				LearningMethod.SUBTRACTIVE,
-				0.1);
+		final CompetitiveTraining train = new CompetitiveTraining(network, 0.4,
+				training, new NeighborhoodSingle());
 
 		int iteration = 0;
 
 		for (iteration = 0; iteration <= 100; iteration++) {
 			train.iteration();
+			System.out.println(train.getError());
 		}
 
 		final NeuralData data1 = new BasicNeuralData(
@@ -55,7 +71,7 @@ public class TestCompetitive {
 		int result1 = network.winner(data1);
 		int result2 = network.winner(data2);
 		
-		//Assert.assertTrue(result1!=result2);
+		Assert.assertTrue(result1!=result2);
 
 	}
 
