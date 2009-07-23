18a813eadb7993d6be03215fe2068df0d11da078
Seema Singh <Seemasingh8614@yahoo.com>
2009-07-23 18:27:29 +0000

    Allow ActivationCompetitive to persist.
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@778 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/neural/activation/ActivationCompetitive.java
+++ src/org/encog/neural/activation/ActivationCompetitive.java
@@ -1,8 +1,41 @@
+/*
+ * Encog Artificial Intelligence Framework v2.x
+ * Java Version
+ * http://www.heatonresearch.com/encog/
+ * http://code.google.com/p/encog-java/
+ * 
+ * Copyright 2008-2009, Heaton Research Inc., and individual contributors.
+ * See the copyright.txt in the distribution for a full listing of 
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.encog.neural.activation;
 
 import org.encog.neural.NeuralNetworkError;
 import org.encog.persist.Persistor;
 
+/**
+ * An activation function that only allows a specified number, usually one,  
+ * of the out-bound connection to win.  These connections will share in the 
+ * sim of the output, whereas the other neurons will recieve zero.
+ * 
+ *  This activation function can be useful for "winner take all" layers.
+ *
+ */
 public class ActivationCompetitive extends BasicActivationFunction {
 
 	/**
@@ -10,14 +43,46 @@ public class ActivationCompetitive extends BasicActivationFunction {
 	 */
 	private static final long serialVersionUID = 5396927873082336888L;
 	
+	/**
+	 * How many winning neurons are allowed.
+	 */
 	private int maxWinners = 1;
 
-	public Object clone() {
-		// TODO Auto-generated method stub
-		return new ActivationCompetitive();
+	/**
+	 * Create a competitive activation function with the specified maximum
+	 * number of winners.
+	 * @param winners The maximum number of winners that this function supports.
+	 */
+	public ActivationCompetitive(int winners) {
+		this.maxWinners = winners;
+	}
+	
+	/**
+	 * Create a competitive activation function with one winner allowed.
+	 */
+	public ActivationCompetitive() {
+		this(1);
 	}
 
+
+	/**
+	 * @return A cloned copy of this object.
+	 */
+	@Override
+	public Object clone() {
+		return new ActivationCompetitive(this.maxWinners);
+	}
+
+	/**
+	 * Create a persistor for this object.
+	 */
+	@Override
 	public Persistor createPersistor() {
+		return ActivationCompetitivePersistor();
+	}
+
+	private Persistor ActivationCompetitivePersistor() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -58,6 +123,12 @@ public class ActivationCompetitive extends BasicActivationFunction {
 		}
 	}
 
+	/**
+	 * Implements the activation function.  The array is modified according
+	 * to the activation function being used.  See the class description
+	 * for more specific information on this type of activation function.
+	 * @param d The input array to the activation function.
+	 */
 	public void derivativeFunction(double[] d) {
 		throw new NeuralNetworkError(
 				"Can't use the competitive activation function "
@@ -65,8 +136,19 @@ public class ActivationCompetitive extends BasicActivationFunction {
 		
 	}
 
+	/**
+	 * @return False, indication that no derivative is available for htis
+	 * function.
+	 */
 	public boolean hasDerivative() {
 		return false;
+	}
+
+	/**
+	 * @return The maximum number of winners this function supports.
+	 */
+	public int getMaxWinners() {
+		return this.maxWinners;
 	}
 
 }
