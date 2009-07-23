b8101a5106b414004ed3aefacd4b1b4dc831a43d
Jeff Heaton <jeff@jeffheaton.com>
2009-07-23 10:28:16 +0000

    Created TrainingError exception
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@774 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/neural/networks/training/TrainingError.java
@@ -0,0 +1,31 @@
+package org.encog.neural.networks.training;
+
+import org.encog.neural.NeuralNetworkError;
+
+public class TrainingError extends NeuralNetworkError {
+
+	/**
+	 * The serial id. 
+	 */
+	private static final long serialVersionUID = 9138367057650889570L;
+
+	/**
+	 * Construct a message exception.
+	 * 
+	 * @param msg
+	 *            The exception message.
+	 */
+	public TrainingError(final String msg) {
+		super(msg);
+	}
+
+	/**
+	 * Construct an exception that holds another exception.
+	 * 
+	 * @param t
+	 *            The other exception.
+	 */
+	public TrainingError(final Throwable t) {
+		super(t);
+	}	
+}
