77b148379c5cb9c2875be6e9527759108b85305a
Seema Singh <Seemasingh8614@yahoo.com>
2009-09-18 15:12:51 +0000

    Added OutputFieldGrouped
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@913 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/normalize/output/OutputFieldGrouped.java
@@ -0,0 +1,15 @@
+package org.encog.normalize.output;
+
+public abstract class OutputFieldGrouped implements OutputField {
+	private int group;
+
+	public int getGroup() {
+		return group;
+	}
+
+	public void setGroup(int group) {
+		this.group = group;
+	}
+	
+	
+}
