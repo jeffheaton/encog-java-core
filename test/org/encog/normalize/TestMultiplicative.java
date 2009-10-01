8ddb069edb08c736b0c74b9331de5d9b62a15101
Jeff Heaton <jeff@jeffheaton.com>
2009-10-01 12:06:43 +0000

    beginning of simple unit test for mult normalization
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@961 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ test/org/encog/normalize/TestMultiplicative.java
@@ -0,0 +1,12 @@
+package org.encog.normalize;
+
+import junit.framework.TestCase;
+
+public class TestMultiplicative extends TestCase {
+	double[][] SAMPLE1 = {{-10,5,15},{-2,1,3}};
+	
+	public void testAbsolute()
+	{
+		
+	}
+}
