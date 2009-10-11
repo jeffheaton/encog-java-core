bfdbf29a9b1ab729affe3d3f685bf80e15b67671
Jeff Heaton <jeff@jeffheaton.com>
2009-10-11 02:25:38 +0000

    added to NumberList unit test
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@1003 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- test/org/encog/util/csv/TestNumberList.java
+++ test/org/encog/util/csv/TestNumberList.java
@@ -1,10 +1,27 @@
 package org.encog.util.csv;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class TestNumberList extends TestCase {
-	public void testToNumbers()
+	public void testToList() throws Exception
 	{
-		
+		StringBuilder result = new StringBuilder();
+		double[] doubleData = { 0.5,10000,10.5 };
+		NumberList.toList(CSVFormat.DECIMAL_POINT, result, doubleData);
+		Assert.assertEquals("0.5,10000,10.5", result.toString());
+		result.setLength(0);
+		NumberList.toList(CSVFormat.DECIMAL_COMMA, result, doubleData);
+		Assert.assertEquals("0,5;10000;10,5", result.toString());
 	}
+	
+	public void testFromList() throws Exception
+	{
+		double[] d = NumberList.fromList(CSVFormat.DECIMAL_POINT, "1,2.5,3000");
+		Assert.assertEquals(3, d.length);
+		Assert.assertEquals(1, d[0], 0.1);
+		Assert.assertEquals(2.5, d[1], 0.1);
+		Assert.assertEquals(3000, d[2], 0.1);
+	}
+	
 }
