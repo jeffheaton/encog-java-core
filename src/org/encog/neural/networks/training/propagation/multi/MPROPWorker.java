66ed594b5ca6ea69954359fe75bac9b777b84860
Jeff Heaton <jeff@jeffheaton.com>
2009-10-23 01:02:39 +0000

    More enhancements to MPROP
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@1051 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/neural/networks/training/propagation/multi/MPROPWorker.java
+++ src/org/encog/neural/networks/training/propagation/multi/MPROPWorker.java
@@ -1,7 +1,5 @@
 package org.encog.neural.networks.training.propagation.multi;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.encog.neural.data.NeuralData;
@@ -13,81 +11,172 @@ import org.encog.neural.networks.training.propagation.resilient.ResilientPropaga
 import org.encog.neural.networks.training.propagation.resilient.ResilientPropagationMethod;
 import org.encog.util.ErrorCalculation;
 
-public class MPROPWorker extends Thread {
+/**
+ * Worker process for MPROP training.  Each worker is given a segment of the
+ * training data.  The workers then train individual neural networks based on
+ * this training set.  Results are merged back with the main neural network 
+ * each iteration.
+ *
+ */
+public class MPROPWorker implements Runnable {
 
+	/**
+	 * The object that owns this worker.
+	 */
 	private MultiPropagation owner;
-	private BasicNetwork network;
-	private long high;
-	private long low;
-	private ResilientPropagationMethod method;
-	private PropagationUtil propagationUtil;
-	private final ErrorCalculation errorCalculation = new ErrorCalculation();
-	private double error;
-	private AtomicBoolean done = new AtomicBoolean(false);
-	private Object iterationLock = new Object();
 	
-	public MPROPWorker(BasicNetwork network, MultiPropagation owner, long low,long high)
-	{
+	/**
+	 * The local thread network that is being trained.
+	 */
+	private BasicNetwork network;
+	
+	/**
+	 * The high index point in the training data to be used by this individual
+	 * worker.
+	 */
+	private long high;
+	
+	/**
+	 * The low index point in the training data to be used by this individual
+	 * worker.
+	 */
+	private long low;
+	
+	/**
+	 * The RPROP method being used by this worker.
+	 */
+	private ResilientPropagationMethod method;
+	
+	/**
+	 * The propagation utility being used by this worker.
+	 */
+	private PropagationUtil propagationUtil;
+	
+	/**
+	 * The error calculation object used for this thread.
+	 */
+	private final ErrorCalculation errorCalculation = new ErrorCalculation();
+	
+	/**
+	 * The calculated error for the last iteration of this worker.
+	 */
+	private double error;
+	
+	/**
+	 * Are we done?  If this is true, then this worker should shut down
+	 * at the end of this iteration.
+	 */
+	private AtomicBoolean done = new AtomicBoolean(false);
+	
+	/**
+	 * A lock that is released at the end of each training iteration.
+	 * This allows the iteration in the MultiPropagation object to 
+	 * synchronize with the workers.
+	 */
+	private Object iterationLock = new Object();
+
+	/**
+	 * Construct a MPROP worker.
+	 * @param network The individual network for this worker, this is cloned
+	 * from the master.
+	 * @param owner The MultiPropagation object that this worker belongs to.
+	 * @param low The low training index.
+	 * @param high The high training index.
+	 */
+	public MPROPWorker(BasicNetwork network, MultiPropagation owner, long low,
+			long high) {
 		this.network = network;
 		this.owner = owner;
 		this.low = low;
 		this.high = high;
-		this.method = new ResilientPropagationMethod(ResilientPropagation.DEFAULT_ZERO_TOLERANCE,ResilientPropagation.DEFAULT_MAX_STEP,ResilientPropagation.DEFAULT_INITIAL_UPDATE);
+		this.method = new ResilientPropagationMethod(
+				ResilientPropagation.DEFAULT_ZERO_TOLERANCE,
+				ResilientPropagation.DEFAULT_MAX_STEP,
+				ResilientPropagation.DEFAULT_INITIAL_UPDATE);
 		this.propagationUtil = new PropagationUtil(network, method);
 		this.errorCalculation.reset();
 	}
-	
-	public void iteration()
-	{	
+
+	/**
+	 * Perform one training iteration for this worker.  Called internally
+	 * only.
+	 */
+	private void iteration() {
 		errorCalculation.reset();
 		NeuralDataPair pair = owner.createPair();
-		for(long l = this.low;l<=this.high;l++)
-		{			
-			 owner.getPair(l,pair);
-			 NeuralData actual = this.propagationUtil.forwardPass(pair.getInput());
-			 this.propagationUtil.backwardPass(pair.getIdeal());
-			 errorCalculation.updateError(actual, pair.getIdeal());
+		for (long l = this.low; l <= this.high; l++) {
+			owner.getPair(l, pair);
+			NeuralData actual = this.propagationUtil.forwardPass(pair
+					.getInput());
+			this.propagationUtil.backwardPass(pair.getIdeal());
+			errorCalculation.updateError(actual, pair.getIdeal());
 		}
 		this.setError(errorCalculation.calculateRMS());
 		this.propagationUtil.getMethod().learn();
 		this.owner.updateNetwork(this);
-		this.iterationLock.notifyAll();
-		System.out.println("Iteration " + this.getId());
+		
+		synchronized(this.iterationLock) {
+			this.iterationLock.notifyAll();
+		}
 	}
-	
+
+	/**
+	 * The thread entry point.  This will execute iterations until
+	 * a shutdown is requested.
+	 */
 	public void run() {
-		
+
 		this.done.set(false);
-		
-		while(!this.done.get()) {
+
+		while (!this.done.get()) {
 			iteration();
-		}	
+		}
 		this.owner.notifyShutdown();
 	}
 
+	/**
+	 * @return The error for this worker's last iteration.
+	 */
 	public synchronized double getError() {
 		return error;
 	}
 
+	/**
+	 * Set the error for this worker.
+	 * @param error The error.
+	 */
 	public synchronized void setError(double error) {
 		this.error = error;
 	}
 
+	/**
+	 * @return This worker's local network.
+	 */
 	public BasicNetwork getNetwork() {
 		return network;
 	}
 
+	/**
+	 * Called to request that this worker shut down.  This set's the 
+	 * done instance variable to true.
+	 */
 	public void requestShutdown() {
 		this.done.set(true);
 	}
-	
-	public void waitForIteration()
-	{
-		try {
-			this.iterationLock.wait();
-		} catch (InterruptedException e) {
-			throw new TrainingError(e);
+
+	/**
+	 * Wait for one training iteration to complete in this worker.  This
+	 * allows the main MultiPropagation object to wait for one iteration
+	 * to pass in each of the workers.
+	 */
+	public void waitForIteration() {
+		synchronized (this.iterationLock) {
+			try {
+				this.iterationLock.wait();
+			} catch (InterruptedException e) {
+				throw new TrainingError(e);
+			}
 		}
 	}
-	
+
 }
--- src/org/encog/neural/networks/training/propagation/multi/MultiPropagation.java
+++ src/org/encog/neural/networks/training/propagation/multi/MultiPropagation.java
@@ -1,7 +1,6 @@
 package org.encog.neural.networks.training.propagation.multi;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
 
 import org.encog.neural.data.Indexable;
 import org.encog.neural.data.NeuralDataPair;
@@ -14,18 +13,77 @@ import org.encog.neural.networks.training.BasicTraining;
 import org.encog.neural.networks.training.TrainingError;
 import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
 
+/**
+ * MPROP - Multipropagation Training.  This is a training technique being
+ * developed by Jeff Heaton.  It is meant to be especially optimal for running
+ * on multicore and grid computing systems.
+ * 
+ * This part of Encog is considered "prebeta".  I am very much still tuning it.
+ * 
+ * - Jeff Heaton
+ */
 public class MultiPropagation extends BasicTraining {
 
+	/**
+	 * How many threads are being used to train the network.
+	 */
 	private int threadCount;
+
+	/**
+	 * The workers to be used, one for each thread.
+	 */
 	private MPROPWorker[] workers;
+
+	/**
+	 * The training set to be used. This must be an indexable training set to
+	 * that it can be divided by the threads.
+	 */
 	private Indexable training;
+
+	/**
+	 * The neural network to be trained.
+	 */
 	private BasicNetwork network;
+
+	/**
+	 * Keep track of if the threads have been started or not.
+	 */
 	private boolean threadsStarted;
+
+	/**
+	 * If it is not worthwhile to do MPROP, then we will fall back to using
+	 * RPROP.
+	 */
 	private ResilientPropagation fallback;
+
+	/**
+	 * Lock used to guarantee exclusive access to the getPair method.
+	 */
 	private Object getPairLock = new Object();
+
+	/**
+	 * Lock used to make sure that only one worker is updating the master neural
+	 * network at a time.
+	 */
 	private Object updateLock = new Object();
+
+	/**
+	 * Latch to track all of the threads as they shut down.
+	 */
 	private CountDownLatch shutDownLatch;
 
+	/**
+	 * Construct a MPROP trainer using the specified number of threads. You can
+	 * also call a constructor that determines how many threads to use based on
+	 * the number of processors in the system.
+	 * 
+	 * @param network
+	 *            The network to train.
+	 * @param training
+	 *            The set to use.
+	 * @param threadCount
+	 *            The number of threads to use, must be 1 or higher.
+	 */
 	public MultiPropagation(BasicNetwork network, NeuralDataSet training,
 			int threadCount) {
 		if (!(training instanceof Indexable)) {
@@ -65,24 +123,53 @@ public class MultiPropagation extends BasicTraining {
 		}
 	}
 
+	/**
+	 * Construct a MPROP trainer that will use the number of available
+	 * processors plus 1. If there is only one processor, then threads will not
+	 * be used and this trainer will fall back to RPROP.
+	 * 
+	 * @param network
+	 *            The network to train.
+	 * @param training
+	 *            The training set to use.
+	 */
 	public MultiPropagation(BasicNetwork network, NeuralDataSet training) {
-		this(network, training, 3);
+		this(network, training,
+				(Runtime.getRuntime().availableProcessors() == 1) ? 1 : Runtime
+						.getRuntime().availableProcessors() + 1);
 	}
 
-	public void startThreads() {
+	/**
+	 * Called internally to start the worker threads.
+	 */
+	private void startThreads() {
 		this.threadsStarted = true;
 		for (int i = 0; i < this.workers.length; i++) {
-			this.workers[i].start();
+			Thread t = new Thread(this.workers[i]);
+			t.start();
 		}
 	}
 
+	/**
+	 * @return The trained neural network. Make sure you call "finishTraining"
+	 *         before attempting to access the neural network.  Otherwise you
+	 *         will end up with a reference to a network that is still being
+	 *         updated.
+	 */
 	public BasicNetwork getNetwork() {
 		if (this.fallback != null) {
 			return this.fallback.getNetwork();
-		} else
+		} else {
 			return this.network;
+		}
 	}
 
+	/**
+	 * Perform one iteration of training.  No work is actually done by this
+	 * method, other than providing an indication of what the current error
+	 * level is.  The threads are already running in the background and going
+	 * about their own iterations.  
+	 */
 	public void iteration() {
 
 		if (this.fallback != null) {
@@ -98,26 +185,35 @@ public class MultiPropagation extends BasicTraining {
 		double total = 0;
 		// get an average error
 		for (MPROPWorker worker : this.workers) {
+			worker.waitForIteration();
 			total += worker.getError();
 		}
 		total /= this.workers.length;
 		this.setError(total);
 
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 	}
 
+	/**
+	 * Get one item of training data.  This method will not allocate a new
+	 * NeuralDataPair, rather one should be provided that this method will
+	 * copy into.  To obtain a new NeuralDataPair object of the correct size
+	 * you should call the "createPair" method.
+	 * @param l The desired index.
+	 * @param pair The training data will be copied to this object.
+	 */
 	public void getPair(long l, NeuralDataPair pair) {
 		synchronized (this.getPairLock) {
 			this.training.getRecord(l, pair);
 		}
 	}
 
+	/**
+	 * Create a new neural data pair object of the correct size for the 
+	 * neural network that is being trained. This object will be passed
+	 * to the getPair method to allow the neural data pair objects to be
+	 * copied to it.
+	 * @return A new neural data pair object.
+	 */
 	public NeuralDataPair createPair() {
 		NeuralDataPair result;
 
@@ -135,6 +231,15 @@ public class MultiPropagation extends BasicTraining {
 		return result;
 	}
 
+	/**
+	 * The workers are all training different segments of the training data.
+	 * Each worker has its own neural network that is separate from the
+	 * master neural network stored here.  Calling updateNetwork will "merge"
+	 * the worker's neural network with the master neural network stored here.
+	 * At the end of this process, the master and the worker with both hold
+	 * identical networks that are the result of this merge.
+	 * @param worker The worker to update the master network.
+	 */
 	public void updateNetwork(MPROPWorker worker) {
 		synchronized (this.updateLock) {
 			Double[] workerNet = NetworkCODEC.networkToArray(worker
@@ -151,30 +256,34 @@ public class MultiPropagation extends BasicTraining {
 			NetworkCODEC.arrayToNetwork(masterNet, this.network);
 		}
 	}
-	
+
 	/**
 	 * Should be called after training has completed and the iteration method
 	 * will not be called any further.
 	 * 
-	 * MPROP makes use of multithreading.  It is VERY important that this method
-	 * be called once training is finished.  This method will wait for all 
+	 * MPROP makes use of multithreading. It is VERY important that this method
+	 * be called once training is finished. This method will wait for all
 	 * threads to shut down before exiting.
 	 */
-	public void finishTraining()
-	{
+	public void finishTraining() {
+		// create a latch and wait for workers to shut down
 		this.shutDownLatch = new CountDownLatch(this.workers.length);
-		for(MPROPWorker worker: this.workers)
-		{
+		for (MPROPWorker worker : this.workers) {
 			worker.requestShutdown();
 		}
 		try {
 			this.shutDownLatch.await();
 		} catch (InterruptedException e) {
 		}
+		// restore the training object to a point that training could be
+		// restarted
+		this.threadsStarted = false;
 	}
-	
-	public void notifyShutdown()
-	{
+
+	/**
+	 * Called by workers to notify that that worker has shut down.
+	 */
+	public void notifyShutdown() {
 		this.shutDownLatch.countDown();
 	}
 
