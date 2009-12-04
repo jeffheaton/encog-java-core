3d82654a076319b42bc5edbf803177ec4305299d
Seema Singh <Seemasingh8614@yahoo.com>
2009-12-04 01:00:54 +0000

    Added multi-gaussian neighborhood
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@1140 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/neural/networks/training/competitive/neighborhood/NeighborhoodGaussianMulti.java
@@ -0,0 +1,92 @@
+package org.encog.neural.networks.training.competitive.neighborhood;
+
+import org.encog.util.math.rbf.GaussianFunctionMulti;
+import org.encog.util.math.rbf.RadialBasisFunctionMulti;
+
+public class NeighborhoodGaussianMulti implements NeighborhoodFunction {
+
+	private RadialBasisFunctionMulti rbf;
+	private int[] size;
+	private int[] displacement;
+	
+	NeighborhoodGaussianMulti(int[] size, RadialBasisFunctionMulti rbf)
+	{
+		this.rbf = rbf;
+		this.size = size;
+		calculateDisplacement();
+	}
+	
+	NeighborhoodGaussianMulti(int x,int y,double peak,double width,double center)
+	{
+		int[] size = new int[2];
+		size[0] = x;
+		size[1] = y;
+		
+		double[] centerArray = new double[2];
+		centerArray[0] = center;
+		centerArray[1] = center;
+		
+		double[] widthArray = new double[2];
+		widthArray[0] = width;
+		widthArray[1] = width;
+		
+		RadialBasisFunctionMulti rbf = new GaussianFunctionMulti(peak, centerArray, widthArray);
+		
+		this.rbf = rbf;
+		this.size = size;
+		
+		calculateDisplacement();
+	}
+	
+	private void calculateDisplacement()
+	{
+		this.displacement = new int[this.size.length];
+		for( int i=0;i<this.size.length;i++)
+		{
+			int value;
+			
+			if( i==0 )
+			{
+				value = 0;
+			}
+			else if (i==1)
+			{
+				value = this.size[0];
+			}
+			else
+			{
+				value = this.displacement[i-1] * this.size[i-1];
+			}
+			
+			this.displacement[i] = value;
+		}
+	}
+	
+	private int[] translateCoordinates(int index)
+	{
+		int[] result = new int[displacement.length];
+		int countingIndex = index;
+		
+		for(int i=result.length-1;i>=0;i++)
+		{
+			int value = countingIndex/displacement[i];
+			
+		}
+		
+		return result;
+	}
+	
+	
+	@Override
+	public double function(int currentNeuron, int bestNeuron) {
+		double[] vector = new double[this.displacement.length]; 
+		int[] vectorCurrent = translateCoordinates(currentNeuron);
+		int[] vectorBest = translateCoordinates(bestNeuron);
+		for(int i=0;i<vectorCurrent.length;i++) {
+			vector[i] = vectorCurrent[i]-vectorBest[i];
+		}
+		return rbf.calculate(vector);
+		
+	}
+
+}
