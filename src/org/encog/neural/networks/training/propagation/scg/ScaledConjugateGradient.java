73f227b0c5371bc3fd1898fbd37c793a0a29dd4d
Seema Singh <Seemasingh8614@yahoo.com>
2009-12-21 21:36:24 +0000

    SCG performance
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@1196 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/neural/networks/training/propagation/scg/ScaledConjugateGradient.java
+++ src/org/encog/neural/networks/training/propagation/scg/ScaledConjugateGradient.java
@@ -1,11 +1,34 @@
+/*
+ * Encog Artificial Intelligence Framework v2.x
+ * Java Version
+ * http://www.heatonresearch.com/encog/
+ * http://code.google.com/p/encog-java/
+ * 
+ * Copyright 2008-2009, Heaton Research Inc., and individual contributors.
+ * See the copyright.txt in the distribution for a full listing of 
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.encog.neural.networks.training.propagation.scg;
 
-import org.encog.neural.data.NeuralDataPair;
 import org.encog.neural.data.NeuralDataSet;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.Layer;
 import org.encog.neural.networks.structure.NetworkCODEC;
-import org.encog.neural.networks.training.BasicTraining;
 import org.encog.neural.networks.training.propagation.Propagation;
 import org.encog.neural.networks.training.propagation.gradient.CalculateGradient;
 import org.encog.util.EncogArray;
@@ -53,8 +76,6 @@ public class ScaledConjugateGradient extends Propagation {
 		this.magP = 0;
 		this.restart = false;
 
-		setError(this.network.calculateError(this.training));
-
 		this.weights = NetworkCODEC.networkToArray(this.network);
 		int numWeights = weights.length;
 
@@ -95,6 +116,7 @@ public class ScaledConjugateGradient extends Propagation {
 		for (int i = 0; i < d.length; i++)
 			d[i] *= factor;
 
+		this.setError(prop.getError());
 		return prop.getErrors();
 	}
 
@@ -166,8 +188,6 @@ public class ScaledConjugateGradient extends Propagation {
 
 		NetworkCODEC.arrayToNetwork(weights, this.network);
 
-		// Calculate the new error & gradients of the network.
-		setError(this.network.calculateError(this.training));
 		gradient = calcGradients(weights);
 
 		double gdelta = 2 * delta * (oldError - getError()) / (mu * mu);
