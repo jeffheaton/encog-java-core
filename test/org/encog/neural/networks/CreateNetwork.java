da859f1ff79529770ef6bac10730239a2d817e53
Seema Singh <Seemasingh8614@yahoo.com>
2009-12-21 16:16:43 +0000

    Improvements to unit test.
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@1192 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- test/org/encog/neural/networks/CreateNetwork.java
+++ test/org/encog/neural/networks/CreateNetwork.java
@@ -5,10 +5,9 @@ import org.encog.neural.activation.ActivationSigmoid;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
 import org.encog.neural.networks.structure.NetworkCODEC;
+import org.encog.util.randomize.ConsistentRandomizer;
 
 public class CreateNetwork {
-	
-	public static final double[] RANDOM_NET = {-0.8289675647834567, 0.41428419615431555, -0.6631344291596013, -0.6347844053306126, 0.8725933251770621, 0.20730871363234438, 0.0693984428627592, 0.39495816342847045, 0.2876293823661842, -0.8091007635627903, 0.5170049536924719, -0.8775363794949156, 0.02786434379814584, -0.7373784461103059, 0.7670893161435932};
 	
 	public static BasicNetwork createXORNetworkUntrained()
 	{
@@ -20,7 +19,7 @@ public class CreateNetwork {
 		network.addLayer(new BasicLayer(new ActivationSigmoid(),true,3));
 		network.addLayer(new BasicLayer(new ActivationSigmoid(),true,1));
 		network.getStructure().finalizeStructure();
-		NetworkCODEC.arrayToNetwork(RANDOM_NET, network);
+		(new ConsistentRandomizer(-1,1)).randomize(network);
 		
 		return network;
 	}
