fb61105c44b34d44fa63f42f660001cdc7b30757
Seema Singh <Seemasingh8614@yahoo.com>
2010-02-05 01:51:59 +0000

    Update header for 2.3
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@1256 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/parse/tags/write/WriteXML.java
+++ src/org/encog/parse/tags/write/WriteXML.java
@@ -1,12 +1,12 @@
 /*
- * Encog Artificial Intelligence Framework v2.x
+ * Encog(tm) Artificial Intelligence Framework v2.3
  * Java Version
  * http://www.heatonresearch.com/encog/
  * http://code.google.com/p/encog-java/
  * 
- * Copyright 2008-2010, Heaton Research Inc., and individual contributors.
- * See the copyright.txt in the distribution for a full listing of 
- * individual contributors.
+ * Copyright 2008-2010 by Heaton Research Inc.
+ * 
+ * Released under the LGPL.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -22,7 +22,13 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * Encog and Heaton Research are Trademarks of Heaton Research, Inc.
+ * For information on Heaton Research trademarks, visit:
+ * 
+ * http://www.heatonresearch.com/copyright.html
  */
+
 package org.encog.parse.tags.write;
 
 import java.io.OutputStream;
