ac47b80f528c54ca1d5a501dd0cfb5789b36370b
Seema Singh <Seemasingh8614@yahoo.com>
2010-02-18 20:27:08 +0000

    Added decomposition to matrix package, used some code from JAMA.
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@1274 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/matrix/decomposition/CholeskyDecomposition.java
@@ -0,0 +1,178 @@
+/*
+ * Encog(tm) Artificial Intelligence Framework v2.3
+ * Java Version
+ * http://www.heatonresearch.com/encog/
+ * http://code.google.com/p/encog-java/
+ * 
+ * Copyright 2008-2010 by Heaton Research Inc.
+ * 
+ * Released under the LGPL.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * Encog and Heaton Research are Trademarks of Heaton Research, Inc.
+ * For information on Heaton Research trademarks, visit:
+ * 
+ * http://www.heatonresearch.com/copyright.html
+ */
+package org.encog.matrix.decomposition;
+
+import org.encog.matrix.Matrix;
+import org.encog.matrix.MatrixError;
+
+/**
+ * 
+ * Cholesky Decomposition.
+ * 
+ * For a symmetric, positive definite matrix A, the Cholesky decomposition is an
+ * lower triangular matrix L so that A = L*L'.
+ * 
+ * If the matrix is not symmetric or positive definite, the constructor returns
+ * a partial decomposition and sets an internal flag that may be queried by the
+ * isSPD() method.
+ * 
+ * This file based on a class from the public domain JAMA package.
+ * http://math.nist.gov/javanumerics/jama/
+ */
+public class CholeskyDecomposition implements java.io.Serializable {
+
+	/**
+	 * Serial ID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Array for internal storage of decomposition.
+	 */
+	private double[][] l;
+
+	/**
+	 * Row and column dimension (square matrix).
+	 * 
+	 * @serial matrix dimension.
+	 */
+	private int n;
+
+	/**
+	 * Symmetric and positive definite flag.
+	 * 
+	 * @serial is symmetric and positive definite flag.
+	 */
+	private boolean isspd;
+
+	/**
+	 * Cholesky algorithm for symmetric and positive definite matrix.
+	 * 
+	 * @param matrix
+	 *            Square, symmetric matrix.
+	 */
+
+	public CholeskyDecomposition(final Matrix matrix) {
+
+		// Initialize.
+		double[][] a = matrix.getData();
+		n = matrix.getRows();
+		l = new double[n][n];
+		isspd = (matrix.getCols() == n);
+		// Main loop.
+		for (int j = 0; j < n; j++) {
+			double[] lrowj = l[j];
+			double d = 0.0;
+			for (int k = 0; k < j; k++) {
+				double[] lrowk = l[k];
+				double s = 0.0;
+				for (int i = 0; i < k; i++) {
+					s += lrowk[i] * lrowj[i];
+				}
+				s = (a[j][k] - s) / l[k][k];
+				lrowj[k] = s;
+				d = d + s * s;
+				isspd = isspd & (a[k][j] == a[j][k]);
+			}
+			d = a[j][j] - d;
+			isspd = isspd & (d > 0.0);
+			l[j][j] = Math.sqrt(Math.max(d, 0.0));
+			for (int k = j + 1; k < n; k++) {
+				l[j][k] = 0.0;
+			}
+		}
+	}
+
+
+	/**
+	 * Is the matrix symmetric and positive definite?
+	 * 
+	 * @return true if A is symmetric and positive definite.
+	 */
+
+	public boolean isSPD() {
+		return isspd;
+	}
+
+	/**
+	 * Return triangular factor.
+	 * 
+	 * @return L
+	 */
+
+	public Matrix getL() {
+		return new Matrix(l);
+	}
+
+	/**
+	 * Solve A*X = B.
+	 * 
+	 * @param b
+	 *            A Matrix with as many rows as A and any number of columns.
+	 * @return X so that L*L'*X = b.
+	 */
+	public Matrix solve(final Matrix b) {
+		if (b.getRows() != n) {
+			throw new MatrixError(
+					"Matrix row dimensions must agree.");
+		}
+		if (!isspd) {
+			throw new RuntimeException(
+					"Matrix is not symmetric positive definite.");
+		}
+
+		// Copy right hand side.
+		double[][] x = b.getArrayCopy();
+		int nx = b.getCols();
+
+		// Solve L*Y = B;
+		for (int k = 0; k < n; k++) {
+			for (int j = 0; j < nx; j++) {
+				for (int i = 0; i < k; i++) {
+					x[k][j] -= x[i][j] * l[k][i];
+				}
+				x[k][j] /= l[k][k];
+			}
+		}
+
+		// Solve L'*X = Y;
+		for (int k = n - 1; k >= 0; k--) {
+			for (int j = 0; j < nx; j++) {
+				for (int i = k + 1; i < n; i++) {
+					x[k][j] -= x[i][j] * l[i][k];
+				}
+				x[k][j] /= l[k][k];
+			}
+		}
+
+		return new Matrix(x);
+	}
+}
---
+++ src/org/encog/matrix/decomposition/QRDecomposition.java
@@ -0,0 +1,275 @@
+/*
+ * Encog(tm) Artificial Intelligence Framework v2.3
+ * Java Version
+ * http://www.heatonresearch.com/encog/
+ * http://code.google.com/p/encog-java/
+ * 
+ * Copyright 2008-2010 by Heaton Research Inc.
+ * 
+ * Released under the LGPL.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * Encog and Heaton Research are Trademarks of Heaton Research, Inc.
+ * For information on Heaton Research trademarks, visit:
+ * 
+ * http://www.heatonresearch.com/copyright.html
+ */
+package org.encog.matrix.decomposition;
+
+import org.encog.matrix.Matrix;
+import org.encog.util.math.EncogMath;
+
+/**
+ * QR Decomposition.
+ * <P>
+ * For an m-by-n matrix A with m >= n, the QR decomposition is an m-by-n
+ * orthogonal matrix Q and an n-by-n upper triangular matrix R so that A = Q*R.
+ * <P>
+ * The QR decompostion always exists, even if the matrix does not have full
+ * rank, so the constructor will never fail. The primary use of the QR
+ * decomposition is in the least squares solution of nonsquare systems of
+ * simultaneous linear equations. This will fail if isFullRank() returns false.
+ * 
+ * This file based on a class from the public domain JAMA package.
+ * http://math.nist.gov/javanumerics/jama/
+ */
+
+public class QRDecomposition implements java.io.Serializable {
+
+	/*
+	 * ------------------------ Class variables ------------------------
+	 */
+
+	/**
+	 * Array for internal storage of decomposition.
+	 * 
+	 * @serial internal array storage.
+	 */
+	private double[][] QR;
+
+	/**
+	 * Row and column dimensions.
+	 * 
+	 * @serial column dimension.
+	 * @serial row dimension.
+	 */
+	private int m, n;
+
+	/**
+	 * Array for internal storage of diagonal of R.
+	 * 
+	 * @serial diagonal of R.
+	 */
+	private double[] Rdiag;
+
+	/*
+	 * ------------------------ Constructor ------------------------
+	 */
+
+	/**
+	 * QR Decomposition, computed by Householder reflections.
+	 * 
+	 * @param A
+	 *            Rectangular matrix
+	 * @return Structure to access R and the Householder vectors and compute Q.
+	 */
+
+	public QRDecomposition(Matrix A) {
+		// Initialize.
+		QR = A.getArrayCopy();
+		m = A.getRows();
+		n = A.getCols();
+		Rdiag = new double[n];
+
+		// Main loop.
+		for (int k = 0; k < n; k++) {
+			// Compute 2-norm of k-th column without under/overflow.
+			double nrm = 0;
+			for (int i = k; i < m; i++) {
+				nrm = EncogMath.hypot(nrm, QR[i][k]);
+			}
+
+			if (nrm != 0.0) {
+				// Form k-th Householder vector.
+				if (QR[k][k] < 0) {
+					nrm = -nrm;
+				}
+				for (int i = k; i < m; i++) {
+					QR[i][k] /= nrm;
+				}
+				QR[k][k] += 1.0;
+
+				// Apply transformation to remaining columns.
+				for (int j = k + 1; j < n; j++) {
+					double s = 0.0;
+					for (int i = k; i < m; i++) {
+						s += QR[i][k] * QR[i][j];
+					}
+					s = -s / QR[k][k];
+					for (int i = k; i < m; i++) {
+						QR[i][j] += s * QR[i][k];
+					}
+				}
+			}
+			Rdiag[k] = -nrm;
+		}
+	}
+
+	/*
+	 * ------------------------ Public Methods ------------------------
+	 */
+
+	/**
+	 * Is the matrix full rank?
+	 * 
+	 * @return true if R, and hence A, has full rank.
+	 */
+
+	public boolean isFullRank() {
+		for (int j = 0; j < n; j++) {
+			if (Rdiag[j] == 0)
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Return the Householder vectors
+	 * 
+	 * @return Lower trapezoidal matrix whose columns define the reflections
+	 */
+
+	public Matrix getH() {
+		Matrix X = new Matrix(m, n);
+		double[][] H = X.getData();
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i >= j) {
+					H[i][j] = QR[i][j];
+				} else {
+					H[i][j] = 0.0;
+				}
+			}
+		}
+		return X;
+	}
+
+	/**
+	 * Return the upper triangular factor
+	 * 
+	 * @return R
+	 */
+
+	public Matrix getR() {
+		Matrix X = new Matrix(n, n);
+		double[][] R = X.getData();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i < j) {
+					R[i][j] = QR[i][j];
+				} else if (i == j) {
+					R[i][j] = Rdiag[i];
+				} else {
+					R[i][j] = 0.0;
+				}
+			}
+		}
+		return X;
+	}
+
+	/**
+	 * Generate and return the (economy-sized) orthogonal factor
+	 * 
+	 * @return Q
+	 */
+
+	public Matrix getQ() {
+		Matrix X = new Matrix(m, n);
+		double[][] Q = X.getData();
+		for (int k = n - 1; k >= 0; k--) {
+			for (int i = 0; i < m; i++) {
+				Q[i][k] = 0.0;
+			}
+			Q[k][k] = 1.0;
+			for (int j = k; j < n; j++) {
+				if (QR[k][k] != 0) {
+					double s = 0.0;
+					for (int i = k; i < m; i++) {
+						s += QR[i][k] * Q[i][j];
+					}
+					s = -s / QR[k][k];
+					for (int i = k; i < m; i++) {
+						Q[i][j] += s * QR[i][k];
+					}
+				}
+			}
+		}
+		return X;
+	}
+
+	/**
+	 * Least squares solution of A*X = B
+	 * 
+	 * @param B
+	 *            A Matrix with as many rows as A and any number of columns.
+	 * @return X that minimizes the two norm of Q*R*X-B.
+	 * @exception IllegalArgumentException
+	 *                Matrix row dimensions must agree.
+	 * @exception RuntimeException
+	 *                Matrix is rank deficient.
+	 */
+
+	public Matrix solve(Matrix B) {
+		if (B.getRows() != m) {
+			throw new IllegalArgumentException(
+					"Matrix row dimensions must agree.");
+		}
+		if (!this.isFullRank()) {
+			throw new RuntimeException("Matrix is rank deficient.");
+		}
+
+		// Copy right hand side
+		int nx = B.getCols();
+		double[][] X = B.getArrayCopy();
+
+		// Compute Y = transpose(Q)*B
+		for (int k = 0; k < n; k++) {
+			for (int j = 0; j < nx; j++) {
+				double s = 0.0;
+				for (int i = k; i < m; i++) {
+					s += QR[i][k] * X[i][j];
+				}
+				s = -s / QR[k][k];
+				for (int i = k; i < m; i++) {
+					X[i][j] += s * QR[i][k];
+				}
+			}
+		}
+		// Solve R*X = Y;
+		for (int k = n - 1; k >= 0; k--) {
+			for (int j = 0; j < nx; j++) {
+				X[k][j] /= Rdiag[k];
+			}
+			for (int i = 0; i < k; i++) {
+				for (int j = 0; j < nx; j++) {
+					X[i][j] -= X[k][j] * QR[i][k];
+				}
+			}
+		}
+		return (new Matrix(X).getMatrix(0, n - 1, 0, nx - 1));
+	}
+}
