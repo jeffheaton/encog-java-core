0c7ff86b4bc5f9558123dcbc61a94024a38a70ba
Jeff Heaton <jeff@jeffheaton.com>
2010-03-20 10:57:53 +0000

    Added deg/radian conv
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@1389 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/mathutil/EncogMath.java
+++ src/org/encog/mathutil/EncogMath.java
@@ -46,4 +46,14 @@ public class EncogMath {
 	      }
 	      return r;
 	   }
+	   
+	   public static double deg2rad(double deg)
+	   {
+		   return deg * (Math.PI/180.0);
+	   }
+	   
+	   public static double rad2deg(double rad)
+	   {
+		   return rad * (180.0/Math.PI);
+	   }
 }
