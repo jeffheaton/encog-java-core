3b66b236dfc298e4050f90bca8e7e0e957c679e4
Seema Singh <Seemasingh8614@yahoo.com>
2010-07-27 17:13:54 +0000

    moved more to basic matrix.
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@1835 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/mathutil/matrices/BasicMatrix.java
+++ src/org/encog/mathutil/matrices/BasicMatrix.java
@@ -354,7 +354,374 @@ public abstract class BasicMatrix implements Matrix {
 		return new Matrix2D(newMatrix);
 	}
 	
+	/**
+	 * Get a submatrix.
+	 * 
+	 * @param i0
+	 *            Initial row index.
+	 * @param i1
+	 *            Final row index.
+	 * @param j0
+	 *            Initial column index.
+	 * @param j1
+	 *            Final column index.
+	 * @return The specified submatrix.
+	 */
+	public Matrix2D getMatrix(final int i0, final int i1, final int j0,
+			final int j1) {
+
+		final Matrix2D result = new Matrix2D(i1 - i0 + 1, j1 - j0 + 1);
+		final double[][] b = result.getData();
+		try {
+			for (int i = i0; i <= i1; i++) {
+				for (int j = j0; j <= j1; j++) {
+					b[i - i0][j - j0] = this.get(i,j);
+				}
+			}
+		} catch (final ArrayIndexOutOfBoundsException e) {
+			throw new MatrixError("Submatrix indices");
+		}
+		return result;
+	}
+
+	/**
+	 * Get a submatrix.
+	 * 
+	 * @param i0
+	 *            Initial row index.
+	 * @param i1
+	 *            Final row index.
+	 * @param c
+	 *            Array of column indices.
+	 * @return The specified submatrix.
+	 */
+	public Matrix2D getMatrix(final int i0, final int i1, final int[] c) {
+		final Matrix2D result = new Matrix2D(i1 - i0 + 1, c.length);
+		final double[][] b = result.getData();
+		try {
+			for (int i = i0; i <= i1; i++) {
+				for (int j = 0; j < c.length; j++) {
+					b[i - i0][j] = get(i,c[j]);
+				}
+			}
+		} catch (final ArrayIndexOutOfBoundsException e) {
+			throw new MatrixError("Submatrix indices");
+		}
+		return result;
+	}
+
+	/**
+	 * Get a submatrix.
+	 * 
+	 * @param r
+	 *            Array of row indices.
+	 * @param j0
+	 *            Initial column index
+	 * @param j1
+	 *            Final column index
+	 * @return The specified submatrix.
+	 */
+	public Matrix2D getMatrix(final int[] r, final int j0, final int j1) {
+		final Matrix2D result = new Matrix2D(r.length, j1 - j0 + 1);
+		final double[][] b = result.getData();
+		try {
+			for (int i = 0; i < r.length; i++) {
+				for (int j = j0; j <= j1; j++) {
+					b[i][j - j0] = get(r[i],j);
+				}
+			}
+		} catch (final ArrayIndexOutOfBoundsException e) {
+			throw new ArrayIndexOutOfBoundsException("Submatrix indices");
+		}
+		return result;
+	}
+
+	/**
+	 * Get a submatrix.
+	 * 
+	 * @param r
+	 *            Array of row indices.
+	 * @param c
+	 *            Array of column indices.
+	 * @return The specified submatrix.
+	 */
+	public Matrix2D getMatrix(final int[] r, final int[] c) {
+		final Matrix2D result = new Matrix2D(r.length, c.length);
+		final double[][] b = result.getData();
+		try {
+			for (int i = 0; i < r.length; i++) {
+				for (int j = 0; j < c.length; j++) {
+					b[i][j] = this.get(r[i],c[j]);
+				}
+			}
+		} catch (final ArrayIndexOutOfBoundsException e) {
+			throw new MatrixError("Submatrix indices");
+		}
+		return result;
+	}
+
+
+	/**
+	 * Get the specified row as a sub-matrix.
+	 * 
+	 * @param row
+	 *            The row to get.
+	 * @return A matrix.
+	 */
+	public Matrix2D getRow(final int row) {
+		if (row > getRows()) {
+			final String str = "Can't get row #" + row
+					+ " because it does not exist.";
+			if (BasicMatrix.LOGGER.isErrorEnabled()) {
+				BasicMatrix.LOGGER.error(str);
+			}
+			throw new MatrixError(str);
+		}
+
+		final double[][] newMatrix = new double[1][getCols()];
+
+		for (int col = 0; col < getCols(); col++) {
+			newMatrix[0][col] = this.get(row,col);
+		}
+
+		return new Matrix2D(newMatrix);
+	}
+
+	/**
+	 * Compute a hash code for this matrix.
+	 * 
+	 * @return The hash code.
+	 */
+	@Override
+	public int hashCode() {
+		long result = 0;
+		for (int r = 0; r < getRows(); r++) {
+			for (int c = 0; c < getCols(); c++) {
+				result += this.get(r,c);
+			}
+		}
+		return (int) (result % Integer.MAX_VALUE);
+	}
+
 	
+	/**
+	 * Determine if the matrix is a vector. A vector is has either a single
+	 * number of rows or columns.
+	 * 
+	 * @return True if this matrix is a vector.
+	 */
+	public boolean isVector() {
+		if (getRows() == 1) {
+			return true;
+		}
+		return getCols() == 1;
+	}
+
+	/**
+	 * Return true if every value in the matrix is zero.
+	 * 
+	 * @return True if the matrix is all zeros.
+	 */
+	public boolean isZero() {
+		for (int row = 0; row < getRows(); row++) {
+			for (int col = 0; col < getCols(); col++) {
+				if ( get(row,col) != 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Multiply every value in the matrix by the specified value.
+	 * 
+	 * @param value
+	 *            The value to multiply the matrix by.
+	 */
+	public void multiply(final double value) {
+
+		for (int row = 0; row < getRows(); row++) {
+			for (int col = 0; col < getCols(); col++) {
+				set(row,col,get(row,col)*value);
+			}
+		}
+	}
+
+	/**
+	 * Multiply every row by the specified vector.
+	 * 
+	 * @param vector
+	 *            The vector to multiply by.
+	 * @param result
+	 *            The result to hold the values.
+	 */
+	public void multiply(final double[] vector, final double[] result) {
+		for (int i = 0; i < getRows(); i++) {
+			result[i] = 0;
+			for (int j = 0; j < getCols(); j++) {
+				result[i] += get(i,j) * vector[j];
+			}
+		}
+	}
+
+	/**
+	 * Set every value in the matrix to the specified value.
+	 * 
+	 * @param value
+	 *            The value to set the matrix to.
+	 */
+	public void set(final double value) {
+		for (int row = 0; row < getRows(); row++) {
+			for (int col = 0; col < getCols(); col++) {
+				set(row,col,value);
+			}
+		}
+
+	}
+
+	/**
+	 * Set an individual cell in the matrix to the specified value.
+	 * 
+	 * @param row
+	 *            The row to set.
+	 * @param col
+	 *            The column to set.
+	 * @param value
+	 *            The value to be set.
+	 */
+	public void set(final int row, final int col, final double value) {
+		validate(row, col);
+		set(row,col,value);
+	}
+
+	/**
+	 * Set this matrix's values to that of another matrix.
+	 * 
+	 * @param matrix
+	 *            The other matrix.
+	 */
+	public void set(final Matrix matrix) {
+		
+		if (matrix instanceof Matrix2D) {
+			final double[][] source = ((Matrix2D)matrix).getData();
+
+			for (int row = 0; row < getRows(); row++) {
+				for (int col = 0; col < getCols(); col++) {
+					set(row,col,source[row][col]);
+				}
+			}
+		} else {
+			for (int row = 0; row < getRows(); row++) {
+				for (int col = 0; col < getCols(); col++) {
+					set(row,col, matrix.get(row, col));
+				}
+			}
+		}
+	}
+	
+
+	/**
+	 * Set a submatrix.
+	 * 
+	 * @param i0
+	 *            Initial row index
+	 * @param i1
+	 *            Final row index
+	 * @param j0
+	 *            Initial column index
+	 * @param j1
+	 *            Final column index
+	 * @param x
+	 *            A(i0:i1,j0:j1)
+	 * 
+	 */
+	public void setMatrix(final int i0, final int i1, final int j0,
+			final int j1, final Matrix x) {
+		try {
+			for (int i = i0; i <= i1; i++) {
+				for (int j = j0; j <= j1; j++) {
+					set(i,j,x.get(i - i0, j - j0));
+				}
+			}
+		} catch (final ArrayIndexOutOfBoundsException e) {
+			throw new MatrixError("Submatrix indices");
+		}
+	}
+
+	/**
+	 * Set a submatrix.
+	 * 
+	 * @param i0
+	 *            Initial row index
+	 * @param i1
+	 *            Final row index
+	 * @param c
+	 *            Array of column indices.
+	 * @param x
+	 *            The submatrix.
+	 */
+
+	public void setMatrix(final int i0, final int i1, final int[] c,
+			final Matrix x) {
+		try {
+			for (int i = i0; i <= i1; i++) {
+				for (int j = 0; j < c.length; j++) {
+					set(i,c[j],x.get(i - i0, j));
+				}
+			}
+		} catch (final ArrayIndexOutOfBoundsException e) {
+			throw new ArrayIndexOutOfBoundsException("Submatrix indices");
+		}
+	}
+
+	/**
+	 * Set a submatrix.
+	 * 
+	 * @param r
+	 *            Array of row indices.
+	 * @param j0
+	 *            Initial column index
+	 * @param j1
+	 *            Final column index
+	 * @param x
+	 *            A(r(:),j0:j1)
+	 */
+
+	public void setMatrix(final int[] r, final int j0, final int j1,
+			final Matrix x) {
+		try {
+			for (int i = 0; i < r.length; i++) {
+				for (int j = j0; j <= j1; j++) {
+					set(r[i],j,x.get(i, j - j0));
+				}
+			}
+		} catch (final ArrayIndexOutOfBoundsException e) {
+			throw new ArrayIndexOutOfBoundsException("Submatrix indices");
+		}
+	}
+
+	/**
+	 * Set a submatrix.
+	 * 
+	 * @param r
+	 *            Array of row indices.
+	 * @param c
+	 *            Array of column indices.
+	 * @param x
+	 *            The matrix to set.
+	 */
+	public void setMatrix(final int[] r, final int[] c, final Matrix x) {
+		try {
+			for (int i = 0; i < r.length; i++) {
+				for (int j = 0; j < c.length; j++) {
+					set(r[i],c[j],x.get(i, j));
+				}
+			}
+		} catch (final ArrayIndexOutOfBoundsException e) {
+			throw new MatrixError("Submatrix indices");
+		}
+	}
 
 	
 }
