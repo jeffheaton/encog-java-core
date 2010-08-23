463411656963cb4d54835a08887c7075a8f940e5
Jeff Heaton <jeff@jeffheaton.com>
2010-08-23 12:23:25 +0000

    Added unit tests
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@1867 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ test/org/encog/neural/networks/TestBiasActivation.java
@@ -0,0 +1,76 @@
+package org.encog.neural.networks;
+
+import java.util.Arrays;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.encog.Encog;
+import org.encog.engine.network.flat.FlatNetwork;
+import org.encog.neural.activation.ActivationSigmoid;
+import org.encog.neural.data.NeuralDataSet;
+import org.encog.neural.data.basic.BasicNeuralDataSet;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.layers.Layer;
+import org.encog.neural.networks.structure.NetworkCODEC;
+import org.encog.neural.networks.training.Train;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
+import org.encog.util.logging.Logging;
+import org.encog.util.simple.EncogUtility;
+
+public class TestBiasActivation extends TestCase {
+	
+	public void testLayerOutput()
+	{
+		Layer hidden, output;
+		BasicNetwork network = new BasicNetwork();
+		network.addLayer(new BasicLayer(null, false,2));
+		network.addLayer(hidden = new BasicLayer(new ActivationSigmoid(), true,4));
+		network.addLayer(output = new BasicLayer(new ActivationSigmoid(), true,1));
+		network.reset();
+		hidden.setBiasActivation(0.5);
+		output.setBiasActivation(-1.0);
+		network.getStructure().finalizeStructure();
+		
+		FlatNetwork flat = network.getStructure().getFlat();
+		
+		Assert.assertNotNull(flat);
+		double[] layerOutput = flat.getLayerOutput();
+		Assert.assertEquals(layerOutput[5], -1.0);
+		Assert.assertEquals(layerOutput[8], 0.5);	
+	}
+	
+	public void testTrain()
+	{
+		Logging.stopConsoleLogging();
+		BasicNetwork network1 = NetworkUtil.createXORNetworkUntrained();
+		BasicNetwork network2 = (BasicNetwork)network1.clone();
+		BasicNetwork network3 = (BasicNetwork)network1.clone();
+		network2.setBiasActivation(-1);
+		network2.getStructure().finalizeStructure();
+		network3.setBiasActivation(0.5);
+		network3.getStructure().finalizeStructure();
+		
+		NeuralDataSet trainingData = new BasicNeuralDataSet(XOR.XOR_INPUT,XOR.XOR_IDEAL);
+		
+		Train rprop1 = new ResilientPropagation(network1, trainingData);
+		Train rprop2 = new ResilientPropagation(network2, trainingData);
+		Train rprop3 = new ResilientPropagation(network3, trainingData);
+
+		NetworkUtil.testTraining(rprop1,0.03);
+		NetworkUtil.testTraining(rprop2,0.01);
+		NetworkUtil.testTraining(rprop3,0.01);
+		
+		network1.getStructure().updateFlatNetwork();
+		network2.getStructure().updateFlatNetwork();
+		network3.getStructure().updateFlatNetwork();
+		
+		double[] w1 = NetworkCODEC.networkToArray(network1);
+		double[] w2 = NetworkCODEC.networkToArray(network2);
+		double[] w3 = NetworkCODEC.networkToArray(network3);
+		
+		Assert.assertTrue(Math.abs(w1[0]-w2[0])>Encog.DEFAULT_DOUBLE_EQUAL);
+		Assert.assertTrue(Math.abs(w2[0]-w3[0])>Encog.DEFAULT_DOUBLE_EQUAL);
+		
+	}
+}
---
+++ test/org/encog/neural/networks/TestFlatIntegration.java
@@ -0,0 +1,30 @@
+package org.encog.neural.networks;
+
+import org.encog.neural.data.NeuralData;
+import org.encog.neural.data.basic.BasicNeuralData;
+import org.encog.neural.networks.structure.FlatUpdateNeeded;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class TestFlatIntegration extends TestCase {
+
+	public void testNetworkOutput()
+	{
+		BasicNetwork network1 = NetworkUtil.createXORNetworkUntrained();
+		network1.getStructure().finalizeStructure();
+		Assert.assertNotNull(network1.getStructure().getFlat());
+		Assert.assertEquals(network1.getStructure().getFlatUpdate(),FlatUpdateNeeded.None);
+		double[] inputArray = {1.0};
+		NeuralData input = new BasicNeuralData(inputArray);
+		
+		// using a holder will cause the network to calculate without the flat network,
+		// should calculate to exactly the same number, with or without flat.
+		NeuralOutputHolder holder = new NeuralOutputHolder();
+		NeuralData output1 = network1.compute(input);
+		NeuralData output2 = network1.compute(input,holder);
+		int i1 = (int)(output1.getData(0) * 10000);
+		int i2 = (int)(output2.getData(0) * 10000);
+		Assert.assertEquals(i1, i2);
+	}
+}
