27ebcdc32a1e78c14f3d6d5534800be5e8ce3ca2
Seema Singh <Seemasingh8614@yahoo.com>
2010-08-23 21:23:29 +0000

    Javascript unit test
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@1883 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ test/org/encog/script/TestJavaScript.java
@@ -0,0 +1,13 @@
+package org.encog.script;
+
+import junit.framework.TestCase;
+
+public class TestJavaScript extends TestCase {
+
+	public void testHelloWorld()
+	{
+		EncogScript script = new EncogScript();
+		script.setSource("print(\'Hello World\')\n");
+		script.run();
+	}
+}
