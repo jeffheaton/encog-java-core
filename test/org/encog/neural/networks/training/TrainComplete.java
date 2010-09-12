3b3dc09ebdd4c13efc045210f334810937317f9a
Jeff Heaton <jeff@jeffheaton.com>
2010-09-12 02:05:45 +0000

    Added unit test to verify conv rate.
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@2008 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ test/org/encog/neural/networks/training/TrainComplete.java
@@ -0,0 +1,36 @@
+package org.encog.neural.networks.training;
+
+import org.encog.mathutil.randomize.ConsistentRandomizer;
+import org.encog.mathutil.randomize.Randomizer;
+import org.encog.neural.data.NeuralDataSet;
+import org.encog.neural.data.basic.BasicNeuralDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.NetworkUtil;
+import org.encog.neural.networks.XOR;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
+import org.encog.util.logging.Logging;
+import org.encog.util.simple.EncogUtility;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class TrainComplete extends TestCase {
+	
+	public void testCase()
+	{
+		Logging.stopConsoleLogging();
+		NeuralDataSet trainingData = new BasicNeuralDataSet(XOR.XOR_INPUT,XOR.XOR_IDEAL);
+		
+		BasicNetwork network = EncogUtility.simpleFeedForward(2, 5, 7, 1, true);
+		Randomizer randomizer = new ConsistentRandomizer(-1, 1, 50);
+		randomizer.randomize(network);
+		Train rprop = new ResilientPropagation(network, trainingData);
+		int iteration = 0;
+		do {
+			rprop.iteration();
+			iteration++;
+		} while( iteration<5000 && rprop.getError()>0.01);
+		Assert.assertTrue(iteration<40);
+	}
+	
+}
