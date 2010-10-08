7b20b62358febac28d85be07c934c08940f0b348
Seema Singh <Seemasingh8614@yahoo.com>
2010-10-08 19:39:29 +0000

    Added Mexican hat RBF
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@2172 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- test/org/encog/neural/networks/training/competitive/neighborhood/TestNeighborhood.java
+++ test/org/encog/neural/networks/training/competitive/neighborhood/TestNeighborhood.java
@@ -48,15 +48,10 @@ public class TestNeighborhood extends TestCase {
 	
 	public void testGaussian() throws Throwable {
 		RadialBasisFunction radial = new GaussianFunction(0.0,1.0,1.0);
-		NeighborhoodGaussian bubble = new NeighborhoodGaussian(radial);
+		NeighborhoodSingleRBF bubble = new NeighborhoodSingleRBF(radial);
 		Assert.assertEquals(0.0, bubble.function(5, 0),0.1);
 		Assert.assertEquals(1.0, bubble.function(5, 5),0.1);
 		Assert.assertEquals(0.6, bubble.function(5, 4),0.1);
 	}
-	
-	public void testGaussianMulti() throws Throwable {
-		NeighborhoodGaussianMulti n = new NeighborhoodGaussianMulti(5,5);
-		Assert.assertEquals(0.6, n.function(5, 0),0.1);
-		Assert.assertEquals(1.0, n.function(5, 5),0.1);
-	}
+
 }
