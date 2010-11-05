227296b1cedd31b6fb1fdb67fd0882f5d5df4853
Jeff Heaton <jeff@jeffheaton.com>
2010-11-05 10:37:30 +0000

    Fixed issue where RBF network had a bias.
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@2253 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/engine/network/flat/FlatNetworkRBF.java
+++ src/org/encog/engine/network/flat/FlatNetworkRBF.java
@@ -73,7 +73,7 @@ public class FlatNetworkRBF extends FlatNetwork implements Serializable {
 
 		layers[0] = new FlatLayer(new ActivationLinear(), inputCount, 0.0,
 				slope);
-		layers[1] = new FlatLayer(new ActivationLinear(), hiddenCount, 1.0,
+		layers[1] = new FlatLayer(new ActivationLinear(), hiddenCount, 0.0,
 				slope);
 		layers[2] = new FlatLayer(new ActivationLinear(), outputCount, 0.0,
 				slope);
