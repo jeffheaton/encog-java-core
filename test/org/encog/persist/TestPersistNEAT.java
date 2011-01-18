3ca58594dc0bf11698e5ca4c1a13964fcefd3c2e
Seema Singh <Seemasingh8614@yahoo.com>
2011-01-18 21:17:21 +0000

    Unit tests for NEAT persistence
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@2464 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- test/org/encog/persist/TestPersistNEAT.java
+++ test/org/encog/persist/TestPersistNEAT.java
@@ -25,7 +25,9 @@ package org.encog.persist;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
@@ -33,8 +35,10 @@ import junit.framework.TestCase;
 import org.encog.engine.network.activation.ActivationFunction;
 import org.encog.engine.network.activation.ActivationStep;
 import org.encog.neural.art.ART1;
+import org.encog.neural.neat.NEATLink;
 import org.encog.neural.neat.NEATNetwork;
 import org.encog.neural.neat.NEATNeuron;
+import org.encog.neural.neat.NEATNeuronType;
 import org.encog.util.obj.SerializeObject;
 
 public class TestPersistNEAT extends TestCase {
@@ -48,12 +52,71 @@ public class TestPersistNEAT extends TestCase {
 		List<NEATNeuron> neurons = new ArrayList<NEATNeuron>();
 		ActivationFunction af = new ActivationStep();
 		
+		// create the neurons
+		NEATNeuron input1 = new NEATNeuron(
+				NEATNeuronType.Input, 
+				1,
+				0.1, 
+				0.2,
+				0.3);
+		
+		NEATNeuron input2 = new NEATNeuron(
+				NEATNeuronType.Input, 
+				2,
+				0.1, 
+				0.2,
+				0.3);
+		
+		NEATNeuron bias = new NEATNeuron(
+				NEATNeuronType.Bias, 
+				3,
+				0.1, 
+				0.2,
+				0.3);
+		
+		NEATNeuron hidden1 = new NEATNeuron(
+				NEATNeuronType.Hidden, 
+				4,
+				0.1, 
+				0.2,
+				0.3);
+		
+		NEATNeuron output = new NEATNeuron(
+				NEATNeuronType.Output, 
+				5,
+				0.1, 
+				0.2,
+				0.3);
+
+		// add the neurons
+		neurons.add(input1);
+		neurons.add(input2);
+		neurons.add(hidden1);
+		neurons.add(bias);
+		neurons.add(output);
+		
+		// connect everything
+		link(0.01, input1, hidden1, false);
+		link(0.01, input2, hidden1, false);
+		link(0.01, bias, hidden1, false);
+		link(0.01, hidden1, output, false);
+		
+		// create the network
 		NEATNetwork result = new NEATNetwork(2, 
 				1,
 				neurons,
 				af, 
 				3);
+				
 		return result;
+	}
+	
+	private void link(
+			double weight, NEATNeuron from, NEATNeuron to, boolean recurrent)
+	{
+		NEATLink l = new NEATLink(weight, from, to, recurrent);
+		from.getOutputboundLinks().add(l);
+		to.getInboundLinks().add(l);
 	}
 	
 	public void testPersistEG()
@@ -92,16 +155,29 @@ public class TestPersistNEAT extends TestCase {
 	
 	private void validate(NEATNetwork network)
 	{
-		/*Assert.assertEquals(6, network.getF1Count());
-		Assert.assertEquals(3, network.getF2Count());
-		Assert.assertEquals(18, network.getWeightsF1toF2().size());
-		Assert.assertEquals(18, network.getWeightsF2toF1().size());
-		Assert.assertEquals(2.0, network.getWeightsF1toF2().get(1, 1));
-		Assert.assertEquals(3.0, network.getWeightsF2toF1().get(2, 2));
-		Assert.assertEquals(1.0, network.getA1());
-		Assert.assertEquals(1.5, network.getB1());
-		Assert.assertEquals(5.0, network.getC1());
-		Assert.assertEquals(0.9, network.getD1());
-		Assert.assertEquals(0.9, network.getVigilance());*/
+		Assert.assertEquals(1, network.getOutputCount());
+		Assert.assertEquals(2, network.getInputCount());
+		Assert.assertEquals(3, network.getNetworkDepth());
+		Assert.assertTrue(network.getActivationFunction() instanceof ActivationStep);
+		Assert.assertEquals(0.0, ((ActivationStep)network.getActivationFunction()).getCenter() );
+		Assert.assertEquals(1.0, ((ActivationStep)network.getActivationFunction()).getHigh() );
+		Assert.assertEquals(0.0, ((ActivationStep)network.getActivationFunction()).getLow() );
+		Assert.assertEquals(5,network.getNeurons().size());
+		
+		Map<NEATNeuronType,NEATNeuron> neurons = new HashMap<NEATNeuronType,NEATNeuron>();
+		
+		for(NEATNeuron neuron: network.getNeurons())
+		{
+			neurons.put(neuron.getNeuronType(), neuron);
+		}
+		
+		Assert.assertEquals(4, neurons.size());
+	
+		NEATNeuron output = neurons.get(NEATNeuronType.Output);
+		NEATNeuron input = neurons.get(NEATNeuronType.Input);
+		Assert.assertEquals(1, input.getOutputboundLinks().size());
+		Assert.assertEquals(0, input.getInboundLinks().size());
+		Assert.assertEquals(0, output.getOutputboundLinks().size());
+		Assert.assertEquals(1, output.getInboundLinks().size());
 	}
 }
