7cb33af6cbd00aa59d9b8ad8f9d08ac85f4dfbce
Jeff Heaton <jeff@jeffheaton.com>
2011-01-22 19:17:50 +0000

    Updated all Encog core files to 2011 and v3.0
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@2493 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/ml/genetic/species/BasicSpecies.java
+++ src/org/encog/ml/genetic/species/BasicSpecies.java
@@ -1,9 +1,9 @@
 /*
- * Encog(tm) Core v2.6 - Java Version
+ * Encog(tm) Core v3.0 - Java Version
  * http://www.heatonresearch.com/encog/
  * http://code.google.com/p/encog-java/
  
- * Copyright 2008-2010 Heaton Research, Inc.
+ * Copyright 2008-2011 Heaton Research, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
