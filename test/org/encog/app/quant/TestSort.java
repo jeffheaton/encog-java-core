8bcea1dbed621c0bce6294d3176b13cf5d1de1b5
Jeff Heaton <jeff@jeffheaton.com>
2011-02-10 11:21:25 +0000

    Unit test
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@2581 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ test/org/encog/app/quant/TestSort.java
@@ -0,0 +1,96 @@
+package org.encog.app.quant;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.encog.app.quant.sort.SortCSV;
+import org.encog.app.quant.sort.SortType;
+import org.encog.app.quant.sort.SortedField;
+import org.encog.util.csv.CSVFormat;
+import org.junit.Assert;
+
+import junit.framework.TestCase;
+
+public class TestSort extends TestCase {
+
+	public static final String INPUT_NAME = "test.csv";
+    public static final String OUTPUT_NAME = "test2.csv";
+
+
+    public void generateTestFileHeadings(boolean header) throws IOException
+    {
+    	PrintWriter tw = new PrintWriter(new FileWriter(INPUT_NAME));
+
+        if (header)
+        {
+            tw.println("a,b");
+        }
+
+        tw.println("five,5");
+        tw.println("four,4");
+        tw.println("two,2");
+        tw.println("three,3");                      
+        tw.println("six,6");
+        tw.println("one,1");
+
+        // close the stream
+        tw.close();
+    }
+
+    public void testSortHeaders() throws IOException
+    {
+        generateTestFileHeadings(true);
+        SortCSV norm = new SortCSV();
+        norm.getSortOrder().add(new SortedField(1,SortType.SortString,true));
+        norm.process(INPUT_NAME,OUTPUT_NAME,true,CSVFormat.ENGLISH);
+
+        BufferedReader tr = new BufferedReader(new FileReader(OUTPUT_NAME));
+
+        Assert.assertEquals("\"a\",\"b\"", tr.readLine());
+        Assert.assertEquals("\"one\",1", tr.readLine());
+        Assert.assertEquals("\"two\",2", tr.readLine());
+        Assert.assertEquals("\"three\",3", tr.readLine());
+        Assert.assertEquals("\"four\",4", tr.readLine());
+        Assert.assertEquals("\"five\",5", tr.readLine());
+        Assert.assertEquals("\"six\",6", tr.readLine());
+        Assert.assertNull(tr.readLine());
+
+
+        tr.close();
+
+        (new File("test.csv")).delete();
+        (new File("test2.csv")).delete();
+    }
+    
+    public void testSortNoHeaders() throws IOException
+    {
+        generateTestFileHeadings(false);
+        SortCSV norm = new SortCSV();
+        norm.getSortOrder().add(new SortedField(1,SortType.SortInteger,true));
+        norm.process(INPUT_NAME,OUTPUT_NAME,false,CSVFormat.ENGLISH);
+
+        BufferedReader tr = new BufferedReader(new FileReader(OUTPUT_NAME));
+
+        Assert.assertEquals("\"one\",1", tr.readLine());
+        Assert.assertEquals("\"two\",2", tr.readLine());
+        Assert.assertEquals("\"three\",3", tr.readLine());
+        Assert.assertEquals("\"four\",4", tr.readLine());
+        Assert.assertEquals("\"five\",5", tr.readLine());
+        Assert.assertEquals("\"six\",6", tr.readLine());
+        Assert.assertNull(tr.readLine());
+
+
+        tr.close();
+
+        (new File("test.csv")).delete();
+        (new File("test2.csv")).delete();
+    }
+
+
+	
+	
+}
