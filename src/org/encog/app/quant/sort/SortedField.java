3a0e57e9bfe71448a55f8a85e53f8fa53a6df681
Jeff Heaton <jeff@jeffheaton.com>
2011-02-10 11:20:44 +0000

    Added sort norm
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@2580 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/app/quant/sort/SortedField.java
@@ -0,0 +1,80 @@
+package org.encog.app.quant.sort;
+
+/**
+ *  Specifies how a field is to be sorted by SortCSV.
+ */
+public class SortedField {
+
+	/**
+	 * The index of the field.
+	 */
+    private int index;
+
+    /**
+     * True, if the field is to be sorted ascending.
+     */
+    private boolean ascending;
+
+    /**
+     * The type of sort.
+     */
+    private SortType sortType;
+
+    /**
+     * Construct the object.
+     * @param index The index of the sorted field.
+     * @param t The type of sort, the type of object.
+     * @param ascending True, if this is an ascending sort.
+     */
+    public SortedField(int index, SortType t, boolean ascending)
+    {
+        this.index = index;
+        this.ascending = ascending;
+        this.sortType = t;
+    }
+
+	/**
+	 * @return the index
+	 */
+	public int getIndex() {
+		return index;
+	}
+
+	/**
+	 * @param index the index to set
+	 */
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	/**
+	 * @return the ascending
+	 */
+	public boolean isAscending() {
+		return ascending;
+	}
+
+	/**
+	 * @param ascending the ascending to set
+	 */
+	public void setAscending(boolean ascending) {
+		this.ascending = ascending;
+	}
+
+	/**
+	 * @return the sortType
+	 */
+	public SortType getSortType() {
+		return sortType;
+	}
+
+	/**
+	 * @param sortType the sortType to set
+	 */
+	public void setSortType(SortType sortType) {
+		this.sortType = sortType;
+	}
+    
+    
+	
+}
