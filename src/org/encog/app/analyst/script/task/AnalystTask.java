f99edc36fcba42385c0b47f1555606d065cb6e6e
Jeff Heaton <jeff@jeffheaton.com>
2011-02-22 23:30:57 +0000

    Added AnalystTask
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@2681 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/app/analyst/script/task/AnalystTask.java
@@ -0,0 +1,32 @@
+package org.encog.app.analyst.script.task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnalystTask {
+
+	
+	private String name;
+	private final List<String> lines = new ArrayList<String>();
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the lines
+	 */
+	public List<String> getLines() {
+		return lines;
+	}
+	
+	
+}
--- src/org/encog/app/analyst/task/AnalystTask.java
+++
@@ -1,8 +0,0 @@
-package org.encog.app.analyst.task;
-
-public class AnalystTask {
-	private String name;
-	private String commands;
-	
-	
-}
