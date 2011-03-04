4c773dfd7b701e116015d8803991dbe6ef17356c
Jeff Heaton <jeff@jeffheaton.com>
2011-03-04 02:37:19 +0000

    CSV can now parse fixed width
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@2767 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ test/org/encog/util/csv/TestReadCSV.java
@@ -0,0 +1,57 @@
+package org.encog.util.csv;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class TestReadCSV extends TestCase {
+	
+	public static final String INPUT_NAME = "test.csv";
+
+	public void testCSVComma() throws IOException
+	{
+		PrintWriter out = new PrintWriter(new FileWriter(INPUT_NAME));
+		out.println("one,1");
+		out.println("two,2");
+		out.println("three,3");
+		out.close();
+		
+		ReadCSV csv = new ReadCSV(INPUT_NAME,false,CSVFormat.EG_FORMAT);
+		csv.next();
+		Assert.assertEquals("one", csv.get(0));
+		Assert.assertEquals("1", csv.get(1));
+		csv.next();
+		Assert.assertEquals("two", csv.get(0));
+		Assert.assertEquals("2", csv.get(1));
+		csv.next();
+		Assert.assertEquals("three", csv.get(0));
+		Assert.assertEquals("3", csv.get(1));
+		Assert.assertFalse(csv.next());
+		csv.close();
+	}
+	
+	public void testCSVSpace() throws IOException
+	{
+		PrintWriter out = new PrintWriter(new FileWriter(INPUT_NAME));
+		out.println("one 1");
+		out.println("two\t2");
+		out.println("three  3");
+		out.close();
+		
+		ReadCSV csv = new ReadCSV(INPUT_NAME,false,new CSVFormat('.',' '));
+		csv.next();
+		Assert.assertEquals("one", csv.get(0));
+		Assert.assertEquals("1", csv.get(1));
+		csv.next();
+		Assert.assertEquals("two", csv.get(0));
+		Assert.assertEquals("2", csv.get(1));
+		csv.next();
+		Assert.assertEquals("three", csv.get(0));
+		Assert.assertEquals("3", csv.get(1));
+		Assert.assertFalse(csv.next());
+		csv.close();
+	}
+}
