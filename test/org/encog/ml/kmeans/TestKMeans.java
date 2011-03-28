15d6cd7e8c2bd2f88228dc307bc8346edbbb58bc
Seema Singh <Seemasingh8614@yahoo.com>
2011-03-28 17:25:44 +0000

    Added kmeans cluster test
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@2976 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ test/org/encog/ml/kmeans/TestKMeans.java
@@ -0,0 +1,58 @@
+package org.encog.ml.kmeans;
+
+import java.util.Arrays;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.encog.engine.data.BasicEngineData;
+import org.encog.engine.data.EngineData;
+import org.encog.engine.data.EngineDataSet;
+import org.encog.neural.data.basic.BasicNeuralData;
+import org.encog.neural.data.basic.BasicNeuralDataSet;
+
+public class TestKMeans extends TestCase {
+	public static final double[][] DATA = {
+        {28,15,22},
+        {16,15,32},
+        {32,20,44},
+        {1,2,3},
+        {3,2,1} };
+	
+    public void testCluster (){
+                
+        BasicNeuralDataSet set = new BasicNeuralDataSet();
+        
+        for(int i=0;i<DATA.length;i++)
+        {
+        	set.add(new BasicNeuralData(DATA[i]));
+        }
+
+        KMeansClustering kmeans = new KMeansClustering(2,set);
+        
+        kmeans.iteration(100);
+        Assert.assertEquals(37, (int)kmeans.getWCSS());
+                              
+        int i = 1;
+        for(KMeansCluster cluster: kmeans.getClusters())
+        {
+        	EngineDataSet ds = cluster.createDataSet();
+            EngineData pair = BasicEngineData.createPair(ds.getInputSize(), ds.getIdealSize());
+            for(int j=0;j<ds.getRecordCount();j++)
+            {
+            	ds.getRecord(j, pair);
+            	for(j=0;j<pair.getInputArray().length;j++) {
+            		if(i==1) {
+            			Assert.assertTrue(pair.getInputArray()[j]>10);
+            		} else {
+            			Assert.assertTrue(pair.getInputArray()[j]<10);
+            		}
+            	}
+            	
+            }
+            
+            i++;
+        }       
+    }
+
+}
