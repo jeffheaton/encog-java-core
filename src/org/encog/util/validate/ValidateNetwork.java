c8730a1d2d40bce9ac8adf48c3bb834c63782738
Seema Singh <Seemasingh8614@yahoo.com>
2011-04-04 18:55:52 +0000

    Added validator class to core
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@3079 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/util/validate/ValidateNetwork.java
@@ -0,0 +1,68 @@
+/*
+ * Encog(tm) Workbench v2.6 
+ * http://www.heatonresearch.com/encog/
+ * http://code.google.com/p/encog-java/
+ 
+ * Copyright 2008-2010 Heaton Research, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *   
+ * For more information on Heaton Research copyrights, licenses 
+ * and trademarks visit:
+ * http://www.heatonresearch.com/copyright
+ */
+package org.encog.util.validate;
+
+import org.encog.EncogError;
+import org.encog.ml.MLInput;
+import org.encog.ml.MLMethod;
+import org.encog.ml.MLOutput;
+import org.encog.neural.data.NeuralDataSet;
+
+public class ValidateNetwork {
+	
+	
+	public static void validateMethodToData(MLMethod method, NeuralDataSet training) {
+		
+		if( !(method instanceof MLInput) || !(method instanceof MLOutput) ) {
+			throw new EncogError("This machine learning method is not compatible with the provided data.");
+		}
+		
+		if( !training.isSupervised() && method instanceof MLOutput ) {
+			throw new EncogError("This machine learning method cannot be used with unsupervised data.");			
+		}
+		
+		int trainingInputCount = training.getInputSize();
+		int trainingOutputCount = training.getIdealSize();
+		int methodInputCount = 0;
+		int methodOutputCount = 0;
+		
+		if( method instanceof MLInput ) {
+			methodInputCount = ((MLInput)method).getInputCount();
+		}
+		
+		if( method instanceof MLOutput ) {
+			methodOutputCount = ((MLOutput)method).getOutputCount();
+		}
+		
+		if( methodInputCount != trainingInputCount ) {
+			throw new EncogError("The machine learning method has an input length of " + methodInputCount + ", but the training data has " + trainingInputCount  + ". They must be the same.");
+		}
+		
+		if( methodOutputCount != trainingOutputCount ) {
+			throw new EncogError("The machine learning method has an output length of " + methodOutputCount + ", but the training data has " + trainingOutputCount  + ". They must be the same.");
+		}
+		
+	}
+	
+}
