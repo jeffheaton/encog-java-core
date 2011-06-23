733f482c078ad7c89770211e868daab4ad03dd16
Jeff Heaton <jeff@jeffheaton.com>
2011-06-23 01:55:58 +0000

    Added more docs for RPROP type
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-core@3576 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/main/java/org/encog/neural/flat/train/prop/RPROPType.java
+++ src/main/java/org/encog/neural/flat/train/prop/RPROPType.java
@@ -1,15 +1,32 @@
 package org.encog.neural.flat.train.prop;
 
+/**
+ * Allows the type of RPROP to be defined.  RPROPp is the classic RPROP.
+ * 
+ * For more information, visit:
+ * 
+ * http://www.heatonresearch.com/wiki/RPROP
+ *
+ */
 public enum RPROPType {
 	/**
-	 * RPROP+ : The classic RPROP algorithm.
+	 * RPROP+ : The classic RPROP algorithm.  Uses weight back tracking.
 	 */
 	RPROPp,
 	
 	/**
-	 * RPROP- : 
+	 * RPROP- : No weight back tracking.
 	 */
 	RPROPm,
+	
+	/**
+	 * iRPROP+ : New weight back tracking method, some consider this to be
+	 * the most advanced RPROP.
+	 */
 	iRPROPp,
+	
+	/**
+	 * iRPROP- : New RPROP without weight back tracking. 
+	 */
 	iRPROPm
 }
