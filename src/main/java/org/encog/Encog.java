1442fa3e005d3369e177664855bf1113fae8901f
Jeff Heaton <jeff@jeffheaton.com>
2012-03-23 05:32:44 -0500

    Rolling core to 3.2

--- src/main/java/org/encog/Encog.java
+++ src/main/java/org/encog/Encog.java
@@ -48,7 +48,7 @@ public final class Encog {
 	/**
 	 * The current engog version, this should be read from the properties.
 	 */
-	public static final String VERSION = "3.1.0";
+	public static final String VERSION = "3.2.0";
 
 	/**
 	 * The current engog file version, this should be read from the properties.
