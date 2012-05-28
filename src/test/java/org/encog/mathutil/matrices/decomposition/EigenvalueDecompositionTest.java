79053bcebeb97b9d013273e06c296e82dd8fae6c
Ian Flanigan <flan@lean.to>
2012-05-28 15:52:07 +0200

    Added test for EgenvalueDecomposition

---
+++ src/test/java/org/encog/mathutil/matrices/decomposition/EigenvalueDecompositionTest.java
@@ -0,0 +1,132 @@
+/*
+ * Encog(tm) Core v3.1 - Java Version
+ * http://www.heatonresearch.com/encog/
+ * http://code.google.com/p/encog-java/
+ 
+ * Copyright 2008-2012 Heaton Research, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *   
+ * For more information on Heaton Research copyrights, licenses 
+ * and trademarks visit:
+ * http://www.heatonresearch.com/copyright
+ */
+package org.encog.mathutil.matrices.decomposition;
+
+import static org.junit.Assert.assertEquals;
+
+import org.encog.mathutil.matrices.Matrix;
+import org.junit.Test;
+
+/**
+ * Simple tests for the EigenvalueDecomposition object. 
+ */
+public class EigenvalueDecompositionTest {
+
+	private Matrix matrix1 = new Matrix(
+			new double[][] {
+					{ 2.0, 5.1 },
+					{ -0.5, 0.9 }
+			});
+
+	private Matrix matrix2 = new Matrix(
+			new double[][] {
+					{ 2.0, 5.5 },
+					{ 5.5, -1.0 }
+			});
+
+	@Test
+	public void testEigenvalueDecomposition() {
+		new EigenvalueDecomposition(matrix1);
+	}
+
+	@Test
+	public void testGetD_assymentric() {
+		EigenvalueDecomposition decomposition =
+				new EigenvalueDecomposition(matrix1);
+		Matrix d = decomposition.getD();
+		assertEquals(2, d.getCols());
+		assertEquals(2, d.getRows());
+		assertEquals(1.45, d.get(0, 0), 1e-10);
+		assertEquals(1.4991664351, d.get(0, 1), 1e-10);
+		assertEquals(-1.4991664351, d.get(1, 0), 1e-10);
+		assertEquals(1.45, d.get(1, 1), 1e-10);
+	}
+
+	@Test
+	public void testGetD_symentric() {
+		EigenvalueDecomposition decomposition =
+				new EigenvalueDecomposition(matrix2);
+		Matrix d = decomposition.getD();
+		assertEquals(2, d.getCols());
+		assertEquals(2, d.getRows());
+		assertEquals(-5.2008771255, d.get(0, 0), 1e-10);
+		assertEquals(0.0, d.get(0, 1), 1e-10);
+		assertEquals(0.0, d.get(1, 0), 1e-10);
+		assertEquals(6.2008771255, d.get(1, 1), 1e-10);
+	}
+
+	@Test
+	public void testGetImagEigenvalues_assymentric() {
+		EigenvalueDecomposition decomposition =
+				new EigenvalueDecomposition(matrix1);
+		double[] d = decomposition.getImagEigenvalues();
+		assertEquals(2, d.length);
+		assertEquals(1.4991664351, d[0], 1e-10);
+		assertEquals(-1.4991664351, d[1], 1e-10);
+	}
+
+	@Test
+	public void testGetImagEigenvalues_symentric() {
+		EigenvalueDecomposition decomposition =
+				new EigenvalueDecomposition(matrix2);
+		double[] d = decomposition.getImagEigenvalues();
+		assertEquals(2, d.length);
+		assertEquals(0.0, d[0], 1e-10);
+		assertEquals(0.0, d[1], 1e-10);
+	}
+
+	@Test
+	public void testGetRealEigenvalues_assymentric() {
+		EigenvalueDecomposition decomposition =
+				new EigenvalueDecomposition(matrix1);
+		double[] d = decomposition.getRealEigenvalues();
+		assertEquals(2, d.length);
+		assertEquals(1.45, d[0], 1e-10);
+		assertEquals(1.45, d[1], 1e-10);
+	}
+
+	@Test
+	public void testGetRealEigenvalues_symentric() {
+		EigenvalueDecomposition decomposition =
+				new EigenvalueDecomposition(matrix2);
+		double[] d = decomposition.getRealEigenvalues();
+		assertEquals(2, d.length);
+		assertEquals(-5.2008771255, d[0], 1e-10);
+		assertEquals(6.2008771255, d[1], 1e-10);
+	}
+
+	@Test
+	public void testGetV_assymentric() {
+		EigenvalueDecomposition decomposition =
+				new EigenvalueDecomposition(matrix1);
+		Matrix d = decomposition.getV();
+		assertEquals(2, d.getCols());
+		assertEquals(2, d.getRows());
+		assertEquals(2.9983328701, d.get(0, 0), 1e-10);
+		assertEquals(-1.1, d.get(0, 1), 1e-10);
+		assertEquals(0.0, d.get(1, 0), 1e-10);
+		assertEquals(1.0, d.get(1, 1), 1e-10);
+	}
+
+}
