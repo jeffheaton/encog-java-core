b8047b9620ebca30c4e05add745db40f77f26b9a
Alan Mosca <nitbix@nitbix.com>
2012-07-23 00:09:24 +0100

    added bash job wrappers

---
+++ src/runs/run-environment.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function run() {
+	$do java -cp .:../main/java ensembles.Test $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
+}
+
+function runWithDefaults() {
+	run $1 $2 $3 $4 $5 $6 0.3 $7 $8 $9
+}
+
+function averaging-rprop() {
+	runWithDefaults $1 $2 $3 $4 $5 $6 rprop $7 averaging
+}
+
+function bagging-haberman() {
+	averaging-rprop bagging uci_haberman $1 $2 $3 $4 $5
+}
+
+echo "Compiling source.."
+javac -sourcepath .:../main/java ensembles/Test.java
+
+echo "Encog Ensemble Test suite ready"
+
+
---
+++ src/runs/run-job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. run-environment.sh
+
+function logged_run() {
+	date=$(date +%Y-%m-%d.%H.%M)	
+	$1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} > /home/nitbix/projects/mscproject/runs_output/$date_$1.$!.output
+}
+
+logged_run bagging-haberman 3,30 150,300,1000 0.06 150 mlp:300:sigmoid &
+logged_run bagging-haberman 1,10,100 150,300,1000 0.06 150 mlp:300:sigmoid &
