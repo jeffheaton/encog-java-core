c3dea5004c0f2732b590a8a5161e4bbc59bcb602
Alan Mosca <nitbix@nitbix.com>
2012-07-23 19:25:44 +0100

    merge?

--- src/runs/run-job.sh
+++ src/runs/run-job.sh
@@ -3,8 +3,8 @@
 . run-environment.sh
 
 function logged_run() {
-	date=$(date +%Y-%m-%d.%H.%M)	
-	$1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} > /home/nitbix/projects/mscproject/runs_output/$date_$1.$!.output
+	date=$(date +%Y-%m-%d.%H.%M)
+	($1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} > /home/nitbix/projects/mscproject/runs_output/${date}_$1.${BASHPID}.output)
 }
 
 logged_run bagging-haberman 3,30 150,300,1000 0.06 150 mlp:300:sigmoid &
