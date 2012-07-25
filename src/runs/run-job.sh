cc6e275adaa0c28d8716d97fbb04c7e4f4c09836
Alan Mosca <nitbix@nitbix.com>
2012-07-25 17:09:30 -0400

    update

--- src/runs/run-job.sh
+++ src/runs/run-job.sh
@@ -4,5 +4,5 @@
 
 #logged_run bagging-haberman 3,30 150,300,1000 0.06 150 mlp:300:sigmoid &
 #logged_run bagging-haberman 1,10,100 150,300,1000 0.06 150 mlp:300:sigmoid &
-logged_run boosting-haberman 3,30 150,300,1000 0.06 150 mlp:300:sigmoid &
-logged_run boosting-haberman 1,10,100 150,300,1000 0.06 150 mlp:300:sigmoid &
+logged_run boosting-haberman 3,30 150,300,1000 0.06 150 mlp:300:sigmoid false &
+logged_run boosting-haberman 1,10,100 150,300,1000 0.06 150 mlp:300:sigmoid false &
