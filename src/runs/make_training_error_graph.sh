ac39178e62d1c1b41ead6ad2e1f01fa954ed8ebf
Alan Mosca <nitbix@nitbix.com>
2012-08-15 21:51:28 +0100

    make training graphs with new TrainingCurves app

--- src/runs/make_training_error_graph.sh
+++ src/runs/make_training_error_graph.sh
@@ -14,14 +14,14 @@ function training() {
   kill $pid
 }
 
-training > ~/projects/mscproject/data_plots/training_curves/${technique}-${problem}-${neurons}-1.data
-training > ~/projects/mscproject/data_plots/training_curves/${technique}-${problem}-${neurons}-2.data
-training > ~/projects/mscproject/data_plots/training_curves/${technique}-${problem}-${neurons}-3.data
+training > ~/projects/mscproject/data_plots/training_curves/${problem}-${neurons}-1.data
+training > ~/projects/mscproject/data_plots/training_curves/${problem}-${neurons}-2.data
+training > ~/projects/mscproject/data_plots/training_curves/${problem}-${neurons}-3.data
 
-cat ~/projects/mscproject/data_plots/training_curves/${technique}-${problem}-${neurons}-1.data ~/projects/mscproject/data_plots/training_curves/${technique}-${problem}-${neurons}-2.data ~/projects/mscproject/data_plots/training_curves/${technique}-${problem}-${neurons}-3.data | sort -n > ~/projects/mscproject/data_plots/training_curves/${technique}-${problem}-${neurons}.data
+cat ~/projects/mscproject/data_plots/training_curves/${problem}-${neurons}-1.data ~/projects/mscproject/data_plots/training_curves/${problem}-${neurons}-2.data ~/projects/mscproject/data_plots/training_curves/${problem}-${neurons}-3.data | sort -n > ~/projects/mscproject/data_plots/training_curves/${problem}-${neurons}.data
 
 gnuplot <<EOF
-set title "Training Error ($technique - $problem - $neurons neurons)"
+set title "Training Error ($problem - $neurons neurons)"
 set xlabel "i"
 set ylabel "error"
 plot "~/projects/mscproject/data_plots/training_curves/${problem}-${neurons}.data" with lines using 1:2 title "MSE","~/projects/mscproject/data_plots/training_curves/${problem}-${neurons}.data" with lines using 1:3 title "test misclass.","~/projects/mscproject/data_plots/training_curves/${problem}-${neurons}.data" with lines using 1:4 title "train misclass.",$fixedline
