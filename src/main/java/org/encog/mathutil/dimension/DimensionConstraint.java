b7aea3a3d792288bd7efb7e53f25556ac3574c2d
jheaton <jeff@jeffheaton.com>
2012-09-15 12:09:35 -0500

    Added dimension handler utility class

---
+++ src/main/java/org/encog/mathutil/dimension/DimensionConstraint.java
@@ -0,0 +1,87 @@
+package org.encog.mathutil.dimension;
+
+/**
+ * Specifies a constraint for dimensions, using a lower and upper bound.
+ * 
+ * @author jheaton
+ * 
+ */
+public class DimensionConstraint {
+
+	/**
+	 * The lower bound for each dimension.
+	 */
+	private final MultiDimension lower;
+
+	/**
+	 * The upper bound for each dimension.
+	 */
+	private final MultiDimension upper;
+
+	/**
+	 * Construct the constraint.
+	 * 
+	 * @param n
+	 *            The number of dimensions.
+	 * @param theLower
+	 * @param theUpper
+	 */
+	public DimensionConstraint(int n, int theLower, int theUpper) {
+		this.lower = new MultiDimension(n);
+		this.upper = new MultiDimension(n);
+
+		for (int i = 0; i < n; i++) {
+			this.lower.setDimension(i, theLower);
+			this.upper.setDimension(i, theUpper);
+		}
+	}
+
+	/**
+	 * @return The lower bound for each dimension.
+	 */
+	public MultiDimension getLower() {
+		return lower;
+	}
+
+	/**
+	 * @return The upper bound for each dimension.
+	 */
+	public MultiDimension getUpper() {
+		return upper;
+	}
+
+	/**
+	 * Get the lower bound for a specific dimension.
+	 * 
+	 * @param d
+	 *            The dimension.
+	 * @return The lower bound for the specified dimension.
+	 */
+	public int getLower(int d) {
+		return lower.getDimension(d);
+	}
+
+	/**
+	 * Get the upper bound for a specific dimension.
+	 * 
+	 * @param d
+	 *            The dimension.
+	 * @return The upper bound for the specified dimension.
+	 */
+	public int getUpper(int d) {
+		return upper.getDimension(d);
+	}
+
+	/**
+	 * Get the range (between upper and lower bound) for the specified
+	 * dimension.
+	 * 
+	 * @param d
+	 *            The dimension.
+	 * @return The range for the specified dimension.
+	 */
+	public int getRange(int d) {
+		return upper.getDimension(d) - lower.getDimension(d);
+	}
+
+}
---
+++ src/main/java/org/encog/mathutil/dimension/MultiDimension.java
@@ -0,0 +1,113 @@
+package org.encog.mathutil.dimension;
+
+/**
+ * Handle multi-dimensional integer-based dimensions. Depending on how the
+ * values are interpreted, this structure can be used to either represent a
+ * coordinate, velocity or a constraint.
+ * 
+ * @author jheaton
+ * 
+ */
+public class MultiDimension {
+
+	/**
+	 * The dimensions.
+	 */
+	private final int[] dimensions;
+
+	/**
+	 * Allocate a MultiDimension.
+	 * 
+	 * @param n
+	 *            The number of dimensions.
+	 */
+	public MultiDimension(int n) {
+		this.dimensions = new int[n];
+	}
+
+	/**
+	 * Get a dimension.
+	 * 
+	 * @param d
+	 *            The dimension to get.
+	 * @return The value of the specified dimension.
+	 */
+	public int getDimension(int d) {
+		return this.dimensions[d];
+	}
+
+	/**
+	 * @return The number of dimensions.
+	 */
+	public int size() {
+		return this.dimensions.length;
+	}
+
+	/**
+	 * @return The dimensions as an array.
+	 */
+	public int[] getDimensions() {
+		return dimensions;
+	}
+
+	/**
+	 * Roll the dimension forward by one. Start with the low dimension and tick
+	 * forward. This can be used to iterate through every index position.
+	 * 
+	 * @param constraint
+	 *            The dimension constraints.
+	 * @return True if there are still more combinations, false if we are done.
+	 */
+	public boolean forward(DimensionConstraint constraint) {
+		int i = 0;
+		do {
+			this.dimensions[i]++;
+
+			// is this hidden layer still within the range?
+			if (this.dimensions[i] <= constraint.getUpper(i)) {
+				return true;
+			}
+
+			// increase the next layer if we've maxed out this one
+			this.dimensions[i] = constraint.getLower(i);
+			i++;
+
+		} while (i < size());
+
+		// can't increase anymore, we're done!
+
+		return false;
+	}
+
+	/**
+	 * Flatten the multi-dimensional index into a single dimension index.
+	 * 
+	 * @param constraint
+	 *            The dimension constraints.
+	 * @return The flat 1d index.
+	 */
+	public int flatten(DimensionConstraint constraint) {
+		int result = this.dimensions[0];
+		int mult = 1;
+
+		for (int i = 0; i < size(); i++) {
+			result += (mult * this.dimensions[i]);
+			mult *= constraint.getRange(i);
+		}
+		return result;
+	}
+
+	/**
+	 * Set a single dimension.
+	 * 
+	 * @param d
+	 *            The dimension to set.
+	 * @param value
+	 *            The new value.
+	 */
+	public void setDimension(int d, int value) {
+		this.dimensions[d] = value;
+
+	}
+
+}
