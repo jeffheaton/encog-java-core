3a9573664cf57dfd6fefe158e013ab6ae6943e13
Alan Mosca <nitbix@nitbix.com>
2012-11-28 16:06:34 +0000

    removed runs

--- src/runs/run-job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. run-environment.sh
-
-#logged_run bagging-haberman 3,30 150,300,1000 0.06 150 mlp:300:sigmoid &
-#logged_run bagging-haberman 1,10,100 150,300,1000 0.06 150 mlp:300:sigmoid &
-logged_run boosting-haberman 3,30 150,300,1000 0.06 150 mlp:300:sigmoid false &
-logged_run boosting-haberman 1,10,100 150,300,1000 0.06 150 mlp:300:sigmoid false &
