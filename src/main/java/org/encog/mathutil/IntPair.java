7f3b08f565b2d4fc5c015c7a1d6445b1c98ef970
Leonid Volkov <leonidvolkov@yahoo.com>
2013-01-10 15:28:07 -0600

    New class

---
+++ src/main/java/org/encog/mathutil/IntPair.java
@@ -0,0 +1,47 @@
+package org.encog.mathutil;
+
+public class IntPair {
+	private int x;
+	private int y;
+	
+	public IntPair(int theX, int theY) {
+		this.x = theX;
+		this.y = theY;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void add(int v) {
+		this.x+=v;
+		this.y+=v;
+	}
+	
+	public void addX(int v) {
+		this.x+=v;
+	}
+	
+	public void addY(int v) {
+		this.y+=v;
+	}
+	
+	public void add(int addX, int addY) {
+		this.x+=addX;
+		this.y+=addY;
+	}
+	
+	
+}
