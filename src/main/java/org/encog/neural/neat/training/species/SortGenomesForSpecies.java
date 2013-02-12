3d163f12c508471673af3a4960ed04b4572f54d6
Jeff Heaton <jeff@jeffheaton.com>
2013-02-12 05:32:19 -0600

    Fix TimSort issue in j7

--- src/main/java/org/encog/neural/neat/training/species/SortGenomesForSpecies.java
+++ src/main/java/org/encog/neural/neat/training/species/SortGenomesForSpecies.java
@@ -16,11 +16,13 @@ public class SortGenomesForSpecies implements Comparator<NEATGenome> {
 	
 	@Override
 	public int compare(NEATGenome g1, NEATGenome g2) {
-		if( Math.abs(g1.getAdjustedScore()-g2.getAdjustedScore())<Encog.DEFAULT_DOUBLE_EQUAL ) {
-			return g2.getBirthGeneration()-g1.getBirthGeneration();
-		} else {
-			return train.getSelectionComparator().compare(g1, g2);
+		int result = train.getSelectionComparator().compare(g1, g2);
+		
+		if( result!=0 ) {
+			return result;
 		}
+		
+		return g2.getBirthGeneration()-g1.getBirthGeneration();
 	}
 
 }
