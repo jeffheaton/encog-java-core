916170ef0105a2ab577d65bc44d8877c58b82630
Jeff Heaton <jeff@jeffheaton.com>
2016-01-02 16:50:10 -0600

    Adding scripts that will be used to push from travis ci to nexus

---
+++ .utility/initiate-publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "encog/encog-java-core" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+  if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
+      echo 'Travis can only publish snapshots.'
+      return 0
+  fi
+
+  echo -e "Starting publish to Sonatype...\n"
+
+  ./gradlew uploadArchives -PnexusUsername="${NEXUS_USERNAME}" -PnexusPassword="${NEXUS_PASSWORD}"
+  RETVAL=$?
+
+  if [ $RETVAL -eq 0 ]; then
+    echo 'Completed publish!'
+  else
+    echo 'Publish failed.'
+    return 1
+  fi
+
+fi
