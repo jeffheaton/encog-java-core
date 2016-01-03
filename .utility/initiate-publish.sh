179b9eee350cbbb022453b65acfe4e497f60ec2c
Jeff Heaton <jeff@jeffheaton.com>
2016-01-02 20:52:09 -0600

    fixed sonatype prefix

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -11,7 +11,7 @@ if [ "$TRAVIS_REPO_SLUG" == "encog/encog-java-core" ] && [ "$TRAVIS_PULL_REQUEST
 
   echo -e "Starting publish to Sonatype...\n"
 
-  ./gradlew uploadArchives -PnexusUsername="${NEXUS_USERNAME}" -PnexusPassword="${NEXUS_PASSWORD}"
+  ./gradlew uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
   RETVAL=$?
 
   if [ $RETVAL -eq 0 ]; then
